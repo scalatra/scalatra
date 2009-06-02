a6b37f5ca646e4d5e20568cf37b6d9be61749102
Alan Dipert <alan@ubergibson.com>
2009-06-02 14:18:48 -0400

    shorter, more functional, awesomer - thanks paulp!

--- src/main/scala/Step.scala
+++ src/main/scala/Step.scala
@@ -1,72 +1,63 @@
 package com.thinkminimo.step
 
-import javax.servlet._;
-import javax.servlet.http._;
+import javax.servlet._
+import javax.servlet.http._
 import scala.util.DynamicVariable
 import scala.util.matching.Regex
 import scala.collection.mutable.HashSet
 import scala.collection.jcl.MapWrapper
 
-abstract class Step extends HttpServlet {
-
-  type Params = Map[String,String]
-  type Action = Any=>String
+object Step
+{
+  type Params = Map[String, String]
+  type Action = Any => String
   
-  class Route(val path:String, val action:Action) {
-    private val pattern = ":\\w+"  
+  val protocols = List("GET", "POST", "PUT", "DELETE")
+}
+import Step._
 
-    val names = new Regex(pattern).findAllIn(path).toList
-    val re = new Regex("^"+path.replaceAll(pattern,"(.*?)")+"$")
-
-    def apply(realPath: String): Option[Params]= 
-      re.findFirstMatchIn(realPath) match {
-        case Some(matches) => {
-          Some(Map()++(names zip matches.subgroups))
-        }
-        case None => None 
-      }
-  }
-    
-  val Routes = Map[String,HashSet[Route]](
-    "GET"->new HashSet[Route](), 
-    "POST"->new HashSet[Route](),
-    "PUT"->new HashSet[Route](), 
-    "DELETE"->new HashSet[Route]()
-  )
-
-  val paramsMap = new DynamicVariable[Params](null)
+abstract class Step extends HttpServlet
+{
+  val Routes      = Map(protocols map (_ -> new HashSet[Route]): _*)
+  val paramsMap   = new DynamicVariable[Params](null)
   var contentType = "text/html"
+  
+  class Route(val path: String, val action: Action) {
+    val pattern = """:\w+"""
+    val names = new Regex(pattern) findAllIn path toList
+    val re = new Regex("^%s$" format path.replaceAll(pattern, "(.*?)"))      
 
-  def params(name:String):String = paramsMap.value(name)
+    def apply(realPath: String): Option[Params] =
+      re findFirstMatchIn realPath map (x => Map(names zip x.subgroups : _*))
+  }
 
-  override def service(request:HttpServletRequest, response: HttpServletResponse) {
-
-        val realParams = new MapWrapper[String, Array[String]]() {
-            def underlying = request.getParameterMap.asInstanceOf[java.util.Map[String,Array[String]]]
-        }.map { case(k,v) => k->v(0)}  
-       
-        Routes(request.getMethod).find( route  =>
-          route(request.getRequestURI) match {
-            case Some(args) => {
-              before()_
-              response.setContentType(contentType)
-              paramsMap.withValue(args ++ realParams){
-                response.getWriter.print(route.action())
-              }
-              true
-            }
-            case None => false
-         }) match {
-          case None => response.getWriter.println("Requesting "+ request.getRequestURI+" but only have " + Routes)
-          case Some(pair) => None
+  override def service(request: HttpServletRequest, response: HttpServletResponse) {
+    val realParams = new MapWrapper[String, Array[String]]() {
+      def underlying = request.getParameterMap.asInstanceOf[java.util.Map[String,Array[String]]]
+    }.map { case (k,v) => (k, v(0)) }
+    
+    def isMatchingRoute(route: Route) = {
+      def exec(args: Params) = {
+        before()_
+        response setContentType contentType
+        paramsMap.withValue(args ++ realParams) {
+          response.getWriter print route.action()
         }
-        
+      } 
+      route(request.getRequestURI) map exec isDefined
     }
+    
+    if (Routes(request.getMethod) find isMatchingRoute isEmpty)
+      response.getWriter println "Requesting %s but only have %s".format(request.getRequestURI, Routes)
+  }
   
-  def before()(fun: =>Any) = fun
-  def get(path:String)(fun: =>Any) = Routes("GET") += new Route(path,x=>fun.toString)
-  def post(path:String)(fun: =>Any) = Routes("POST") += new Route(path,x=>fun.toString)
-  def put(path:String)(fun: =>Any) = Routes("PUT") += new Route(path,x=>fun.toString)
-  def delete(path:String)(fun: =>Any) = Routes("DELETE") += new Route(path,x=>fun.toString)
-  
+  def params(name: String): String = paramsMap value name
+  def before()(fun: => Any) = fun
+  val List(get, post, put, delete) = protocols map routeSetter  
+
+  // functional programming means never having to repeat yourself
+  private def routeSetter(protocol: String): (String) => (=> Any) => Unit = {
+    def g(path: String, fun: => Any) = Routes(protocol) += new Route(path, x => fun.toString)
+    (g _).curry
+  }  
 }
