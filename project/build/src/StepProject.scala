dabdeca14df45fa3d3b8bf89b270d64042605c21
Alan Dipert <alan.dipert@gmail.com>
2009-09-28 01:57:44 -0400

    bump scala to 2.7.5, sbt to 0.5.4

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -7,7 +7,6 @@ class StepProject(info: ProjectInfo) extends DefaultWebProject(info)
   override def useMavenConfigurations = true
   override def packageAction = packageTask(mainClasses +++ mainResources, outputPath, defaultJarName, packageOptions).dependsOn(compile) describedAs PackageDescription
 
-  val jetty = "org.mortbay.jetty" % "jetty" % "7.0.0pre3" % "test->default"
   val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "7.0.0pre3" % "test->default"
   val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided->default"
