2a62382136309e60d4575a0971956cca1423fb4a
Ross A. Baker <baker@alumni.indiana.edu>
2010-01-20 00:33:57 -0500

    Remove dependency on platform encoding in StepSuite.
    HttpTester in java-servlet-tester-7.0.0pre3 depends on the platform
    encoding [http://jira.codehaus.org/browse/JETTY-807]. The fix is not yet
    released for jetty-7, but Step can run on jetty-6.
    
    With that fixed, we no longer need the ISO-8859-1 hack in StepSuite.

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -7,7 +7,6 @@ class StepProject(info: ProjectInfo) extends DefaultWebProject(info)
   override def useMavenConfigurations = true
   override def packageAction = packageTask(mainClasses +++ mainResources, outputPath, defaultJarName, packageOptions).dependsOn(compile) describedAs PackageDescription
 
-  val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "7.0.0pre3" % "provided->default"
+  val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided->default"
   val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "provided->default"
-  val servlet = "org.mortbay.jetty" % "servlet-api" % "3.0.pre1" % "provided->default"
 }
