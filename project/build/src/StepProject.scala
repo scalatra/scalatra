114f4bbed513f3df3e3ef33d5456fee243382adc
Ross A. Baker <baker@alumni.indiana.edu>
2010-01-25 23:15:03 -0500

    Upgrade ScalaTest to 1.0.1-for-scala-2.8.0.Beta1-RC7-with-test-interfaces-0.3-SNAPSHOT.

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -8,7 +8,7 @@ class StepProject(info: ProjectInfo) extends DefaultWebProject(info)
   override def packageAction = packageTask(mainClasses +++ mainResources, outputPath, defaultJarName, packageOptions).dependsOn(compile) describedAs PackageDescription
 
   val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "7.0.0pre3" % "provided->default"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-RC5-with-test-interfaces-0.2-SNAPSHOT" % "provided->default"
+  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-RC7-with-test-interfaces-0.3-SNAPSHOT" % "provided->default"
   val servlet = "org.mortbay.jetty" % "servlet-api" % "3.0.pre1" % "provided->default"
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
