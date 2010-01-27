04ba4dda8f09c069def105bd554fb80432721c22
Ross A. Baker <baker@alumni.indiana.edu>
2010-01-27 13:21:29 -0500

    Don't recreate the host:port regex on each request.

--- src/main/scala/Step.scala
+++ src/main/scala/Step.scala
@@ -9,10 +9,14 @@ import scala.collection.jcl.Conversions._
 import scala.xml.NodeSeq
 import Session._
 
-case class StepRequest(r: HttpServletRequest) extends HttpServletRequestWrapper(r) {
-  val p = new Regex("([^:]*):?(.*)")
+object StepRequest {
+  val HostPortRegex = new Regex("([^:]*):?(.*)")
+}
 
-  val List(host, port) = r.getHeader("Host") match { case null => List("",""); case p(x,y) => List(x,y) }
+case class StepRequest(r: HttpServletRequest) extends HttpServletRequestWrapper(r) {
+  import StepRequest._
+
+  val List(host, port) = r.getHeader("Host") match { case null => List("",""); case HostPortRegex(x,y) => List(x,y) }
   def referer = r.getHeader("Referer")
 }
 
