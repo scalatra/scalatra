dc7bfb5eff93e497777a75893c1814fe4f13262c
Ross A. Baker <baker@alumni.indiana.edu>
2010-03-05 13:26:16 -0500

    Use scala-2.8.0.Beta1.
    This branch previously dependended on snapshot only because there was
    not yet a beta release.

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -8,7 +8,7 @@ class StepProject(info: ProjectInfo) extends DefaultWebProject(info)
   override def packageAction = packageTask(mainClasses +++ mainResources, outputPath, defaultJarName, packageOptions).dependsOn(compile) describedAs PackageDescription
 
   val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided->default"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-RC7-with-test-interfaces-0.3-SNAPSHOT" % "provided->default"
+  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "provided->default"
   val mockito = "org.mockito" % "mockito-core" % "1.8.2" % "test"
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
