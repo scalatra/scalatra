55604743c38521ae6037526b02f7265b06032542
Ivan Willig <ivanwillig@mao.local>
2010-03-08 21:51:31 -0500

    add basic support for scalate

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -10,6 +10,7 @@ class StepProject(info: ProjectInfo) extends DefaultWebProject(info)
   val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided->default"
   val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "provided->default"
   val mockito = "org.mockito" % "mockito-core" % "1.8.2" % "test"
-
+  val scalate = "org.fusesource.scalate" % "scalate-core"  % "1.0-SNAPSHOT"
+  val scalateReop = "scalate repo" at "http://scalate.fusesource.org/repo/snapshot/"
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 }
---
+++ src/main/scala/com/thinkminimo/step/Template.scala
@@ -0,0 +1,45 @@
+package com.thinkminimo.step
+import java.io.{File,StringWriter,PrintWriter}
+import org.fusesource.scalate._
+import scala.xml.Node
+
+object Template { 
+	
+	private def makeTemplate(name: String): (StringWriter, DefaultRenderContext, Template) = { 
+		val current = System.getProperty("user.dir");
+	 	val templateFolder  = current+ "/templates/"
+		val engine = new TemplateEngine
+		val template = engine.load(templateFolder + "" + name)  
+		val buffer = new StringWriter
+		val context = new DefaultRenderContext(engine, new PrintWriter(buffer))
+		return (buffer,context,template) 
+		}
+	
+	def render(templateName: String) = { 
+		val (buffer,context,template) =  makeTemplate(templateName)
+		template.render(context) 
+		buffer
+		}
+		
+	def render(templateName: String, variables:(String,Any)*):java.io.StringWriter =  { 
+		val (buffer,context,template) =  makeTemplate(templateName)
+		for (variable <- variables) { 
+			val (key, value) = variable
+			context.attributes(key) = value
+			}
+		template.render(context)
+		buffer
+		}
+	
+	def renderToXHML(title: String, content: Seq[Node]) = { 
+		<html> 
+		  <head> 
+			<title>{title}</title>	
+		  </head>
+		<body>
+			{content}
+		</body>
+		</html>
+		}
+	
+	}
