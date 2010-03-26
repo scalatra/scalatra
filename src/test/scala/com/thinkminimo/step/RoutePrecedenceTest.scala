fc7af29db9018039f05990d7484bf657eb630662
Ross A. Baker <baker@alumni.indiana.edu>
2010-03-26 12:51:34 -0400

    Routes declared later in the same class take precedence.
    This is the opposite of Sinatra, where the earlier route wins.  But to
    do otherwise, while also letting child classes override base classes'
    routes, proves to be difficult in an internal Scala DSL.  Sorry, Sinatra
    users.

--- src/test/scala/com/thinkminimo/step/RoutePrecedenceTest.scala
+++ src/test/scala/com/thinkminimo/step/RoutePrecedenceTest.scala
@@ -12,6 +12,14 @@ class RoutePrecedenceTestChildServlet extends RoutePrecedenceTestBaseServlet {
   get("/override-route") {
     "child"
   }
+
+  get("/hide-route") {
+    "hidden by later route"
+  }
+
+  get("/hide-route") {
+    "visible"
+  }
 }
 
 class RoutePrecedenceTest extends StepSuite with ShouldMatchers {
@@ -20,6 +28,16 @@ class RoutePrecedenceTest extends StepSuite with ShouldMatchers {
   test("Routes in child should override routes in base") {
     get("/override-route") {
       body should equal ("child")
+    }
+  }
+
+  test("Routes declared later in the same class take precedence") {
+    /*
+     * This is the opposite of Sinatra, where the earlier route wins.  But to do otherwise, while also letting child
+     * classes override base classes' routes, proves to be difficult in an internal Scala DSL.  Sorry, Sinatra users.
+     */
+    get("/hide-route") {
+      "visible"
     }
   }
 }
