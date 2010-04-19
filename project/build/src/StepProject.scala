57e19c0eda1da2ed53680d704e4a472a05a49914
Ross A. Baker <baker@alumni.indiana.edu>
2010-04-19 12:59:20 -0400

    Add cross-build support for Scala 2.8.0.RC1 and Scala.2.8.0.Beta1.

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -8,8 +8,16 @@ class StepProject(info: ProjectInfo) extends DefaultWebProject(info)
   override def packageAction = packageTask(mainClasses +++ mainResources, outputPath, defaultJarName, packageOptions).dependsOn(compile) describedAs PackageDescription
 
   val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided->default"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "provided->default"
+  val scalatest = "org.scalatest" % "scalatest" % scalatestVersion(crossScalaVersionString) % "provided->default"
   val mockito = "org.mockito" % "mockito-core" % "1.8.2" % "test"
+
+  def scalatestVersion(scalaVersion: String) = {
+    val qualifier = scalaVersion match {
+      case "2.8.0.Beta1" => "scala-2.8.0.Beta1-with-test-interfaces-0.3"
+      case x => x
+    } 
+    "1.0.1-for-scala-"+qualifier+"-SNAPSHOT"
+  }
 
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 }
