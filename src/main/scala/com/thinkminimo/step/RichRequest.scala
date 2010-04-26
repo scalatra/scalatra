d1a38545e0a02424238cc97dcf579a36e052074c
Miso Korkiakoski <miso.korkiakoski@gmail.com>
2010-04-26 22:13:02 +0300

    Add a shorthand (body) to RichRequest for reading the body
    The body-method follows the Sinatra way of getting the
    request body with "last_request.body".
    Signed-off-by: Miso Korkiakoski <miso.korkiakoski@gmail.com>

--- src/main/scala/com/thinkminimo/step/RichRequest.scala
+++ src/main/scala/com/thinkminimo/step/RichRequest.scala
@@ -1,6 +1,7 @@
 package com.thinkminimo.step
 
 import javax.servlet.http._
+import io.Source
 
 case class RichRequest(r: HttpServletRequest) {
   import RichRequest._
@@ -15,5 +16,10 @@ case class RichRequest(r: HttpServletRequest) {
     case s: String => Some(s)
     case null => None
   }
+
+  def body:String = {
+    Source.fromInputStream(r.getInputStream).getLines.mkString
+  }
+
 }
 
