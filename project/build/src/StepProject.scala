e53844419d617830b71ec0563b52dab98d2f6294
Ross A. Baker <baker@alumni.indiana.edu>
2010-05-02 02:17:49 -0400

    Rename the subprojects to "core" and "example".

--- project/build/src/StepProject.scala
+++ project/build/src/StepProject.scala
@@ -2,12 +2,14 @@ import sbt._
 
 class StepProject(info: ProjectInfo) extends ParentProject(info)
 {
-
-  val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided->default"
-  val scalatest = "org.scalatest" % "scalatest" % "1.0" % "provided->default"
-  val mockito = "org.mockito" % "mockito-core" % "1.8.2" % "test"
-
   override def shouldCheckOutputDirectories = false
-  lazy val stepProject = project(".", "step")
-  lazy val stepExampleProject = project("example-step-project", "example-step-project", new DefaultWebProject(_), stepProject)
+
+  lazy val core = project("core", "step", new CoreProject(_)) 
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) {
+    val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided->default"
+    val scalatest = "org.scalatest" % "scalatest" % "1.0" % "provided->default"
+    val mockito = "org.mockito" % "mockito-core" % "1.8.2" % "test"
+  } 
+
+  lazy val example = project("example", "step-example", new DefaultWebProject(_), core)
 }
