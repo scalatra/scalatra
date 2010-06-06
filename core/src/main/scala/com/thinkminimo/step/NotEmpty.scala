46e4b22deee1ff1ee2846aa7219a201ae109b4f5
Ross A. Baker <baker@alumni.indiana.edu>
2010-06-06 00:48:57 -0400

    Created an extractor for handling empty form field submissions.

---
+++ core/src/main/scala/com/thinkminimo/step/NotEmpty.scala
@@ -0,0 +1,14 @@
+package com.thinkminimo.step
+
+/**
+ * Extractor object, useful for handling empty form parameter submissions:
+ *
+ * params.get("foo") match {
+ *   case NotEmpty(value) => processValue(value)
+ *   case _ => message("foo is required")
+ * }
+ */
+object NotEmpty {
+  def unapply(s: String) = if (s != null && !s.isEmpty) Some(s) else None
+  def unapply(o: Option[String]): Option[String] = o flatMap { s => unapply(s) }
+}
