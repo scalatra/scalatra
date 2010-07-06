cd15e1cbcb97eee9b00b9560667d516ef19dcffa
Ross A. Baker <baker@alumni.indiana.edu>
2010-07-06 01:40:23 -0400

    Provide richer access to the Scalate API.
    The templateEngine is now protected and emulates the
    ServletTemplateEngine.  (The current ServletTemplateEngine
    implementation in Scalate does not support Filters.)  Also, a
    createRenderContext method has been added to provide easy access to a
    ServletRenderContext for the current request and response.

--- scalate/src/main/scala/org/scalatra/step/scalate/ScalateSupport.scala
+++ scalate/src/main/scala/org/scalatra/step/scalate/ScalateSupport.scala
@@ -1,22 +1,61 @@
 package org.scalatra.scalate
 
+import scala.tools.nsc.Global
+import java.io.{File, PrintWriter}
 import javax.servlet.ServletContext
-import org.fusesource.scalate.TemplateEngine
-import org.fusesource.scalate.servlet.{ServletRenderContext, ServletResourceLoader}
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import org.fusesource.scalate.{Binding, RenderContext, TemplateEngine}
+import org.fusesource.scalate.layout.DefaultLayoutStrategy
+import org.fusesource.scalate.servlet.{ServletRenderContext, ServletResourceLoader, ServletTemplateEngine}
+import org.fusesource.scalate.util.ClassPathBuilder
 
 trait ScalateSupport {
   self: {def servletContext: ServletContext} =>
 
   // Laziness lets the servlet context initialize itself first.
-  private lazy val templateEngine = {
-    val result = new TemplateEngine
-    result.resourceLoader = new ServletResourceLoader(servletContext)
-    result
+  //
+  // TODO Convert to a org.fusesource.scalate.servlet.ServletTemplateEngine.  
+  // Currently, ServletTemplateEngine can't be constructed from a Filter.
+  // Going to send them a patch.  In the meantime, we'll just borrow their
+  // implementation.
+  protected lazy val templateEngine = new TemplateEngine {
+    bindings = List(Binding("context", classOf[ServletRenderContext].getName, true, isImplicit = true))  
+
+    // If the scalate.workingdir is not set, then just configure the working
+    // directory under WEB_INF/_scalate
+    if ( System.getProperty("scalate.workingdir", "").length == 0 ) {
+      val path = servletContext.getRealPath("WEB-INF")
+      if (path != null) {
+        workingDirectory = new File(path, "_scalate")
+      }
+    }
+  
+    classpath = buildClassPath
+    resourceLoader = new ServletResourceLoader(servletContext)
+    layoutStrategy = new DefaultLayoutStrategy(this, TemplateEngine.templateTypes.map("/WEB-INF/scalate/layouts/default." + _):_*)
+
+    private def buildClassPath(): String = {
+
+      val builder = new ClassPathBuilder
+
+      // Add containers class path
+      builder.addPathFrom(getClass)
+              .addPathFrom(classOf[ServletContext])
+              .addPathFrom(classOf[Product])
+              .addPathFrom(classOf[Global])
+
+      // Always include WEB-INF/classes and all the JARs in WEB-INF/lib just in case
+      builder.addClassesDir(servletContext.getRealPath("/WEB-INF/classes"))
+              .addLibDir(servletContext.getRealPath("/WEB-INF/lib"))
+
+      builder.classPath
+    }
   }
 
+  def createRenderContext(implicit req: HttpServletRequest, res: HttpServletResponse) = 
+    new ServletRenderContext(templateEngine, req, res, servletContext)
+
   def renderTemplate(path: String, attributes: (String, Any)*)
-                    (implicit request: HttpServletRequest, response: HttpServletResponse) {
-    new ServletRenderContext(templateEngine, request, response, servletContext).render(path, Map(attributes : _*))
-  }
+                    (implicit request: HttpServletRequest, response: HttpServletResponse) = 
+    createRenderContext.render(path, Map(attributes : _*))
 }
