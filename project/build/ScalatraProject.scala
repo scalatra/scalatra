64e788541dc282674d6ec3afd2161d2d3b82a4ee
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-03 01:24:13 -0400

    Remove build-only metadata from generated POMs.

--- project/build/ScalatraProject.scala
+++ project/build/ScalatraProject.scala
@@ -13,37 +13,21 @@ class ScalatraProject(info: ProjectInfo) extends ParentProject(info)
 
   trait ScalatraSubProject extends BasicScalaProject with BasicPackagePaths {
     def description: String
-    def extraPlugins: NodeSeq = Seq.empty
 
     val jettytester = jettyGroupId % "jetty-servlet-tester" % jettyVersion % "provided"
     val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided"
     val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
     val junit = "junit" % "junit" % "4.8.1" % "test"
 
-    override def pomPostProcess(pom: Node) =
-      <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
-        <modelVersion>4.0.0</modelVersion>
-        <parent>
-          <groupId>{organization}</groupId>
-          <artifactId>{crossScalaName(ScalatraProject.this.name)}</artifactId>
-          <version>{version}</version>
-        </parent>
+    override def pomExtra = (
+      <parent>
         <groupId>{organization}</groupId>
-        <artifactId>{crossScalaName(name)}</artifactId>
+        <artifactId>{artifactID}</artifactId>
         <version>{version}</version>
-        {pom \ "packaging"}
-        <name>{name}</name>
-        <description>{description}</description>
-        {pom \ "properties"}
-        <dependencies>
-          {sortDependencies(pom \ "dependencies" \ "dependency")}
-        </dependencies>
-      </project>
-
-    override def makePomConfiguration = 
-      new MakePomConfiguration(deliverProjectDependencies,
-        Some(Configurations.defaultMavenConfigurations),
-        pomExtra, pomPostProcess, pomIncludeRepository)
+      </parent>
+      <name>{name}</name>
+      <description>{description}</description>
+    )
 
     override def packageDocsJar = defaultJarPath("-javadoc.jar")
     override def packageSrcJar= defaultJarPath("-sources.jar")
@@ -71,16 +55,6 @@ class ScalatraProject(info: ProjectInfo) extends ParentProject(info)
   class ScalateProject(info: ProjectInfo) extends DefaultProject(info) with ScalatraSubProject {
     val scalate = "org.fusesource.scalate" % "scalate-core" % "1.2"
     val description = "Supplies the optional Scalatra Scalate support"
-    override val extraPlugins = 
-      <plugin>
-        <artifactId>maven-surefire-plugin</artifactId>
-        <configuration>
-          <excludes>
-            <!-- No tests here, by design.  *.java instead of *.scala also intentional. --> 
-            <exclude>**/TestScalateScalatraFilter.java</exclude>
-          </excludes>
-        </configuration>
-      </plugin>
   }
 
   lazy val example = project("example", "scalatra-example", new ExampleProject(_), core, fileupload, scalate)
@@ -94,234 +68,88 @@ class ScalatraProject(info: ProjectInfo) extends ParentProject(info)
     override def publishAction = Empty
     override def deliverAction = Empty
     override def artifacts = Set.empty
-    override val extraPlugins = 
-      <plugin>
-        <groupId>org.mortbay.jetty</groupId>
-        <artifactId>maven-jetty-plugin</artifactId>
-        <version>{jettyVersion}</version>
-      </plugin>
   }
 
   val fuseSourceSnapshots = "FuseSource Snapshot Repository" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
   val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 
-  override def pomPostProcess(pom: Node) = 
-    <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
-      <modelVersion>4.0.0</modelVersion>
-      <groupId>{organization}</groupId>
-      <artifactId>{crossScalaName(name)}</artifactId>
-      <version>{version}</version>
-      <packaging>pom</packaging>
-      
-      <name>{name}</name>
-      <description>Scalatra Project POM</description>
-  
-      <prerequisites>
-        <maven>2.2.1</maven>
-      </prerequisites>
-  
-      <properties>
-        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-        <scala-version>{crossScalaVersionString}</scala-version>
-        <scaladoc.dir>${{project.build.directory}}/scaladoc</scaladoc.dir>
-      </properties>
-  
+  override def pomExtra = (
+    <name>{name}</name>
+    <description>Scalatra Project POM</description>
+    <url>http://www.scalatra.org/</url>
+    <inceptionYear>2009</inceptionYear>
+    <organization>
+      <name>Scalatra Project</name>
       <url>http://www.scalatra.org/</url>
-      <inceptionYear>2009</inceptionYear>
-  
-      <organization>
-        <name>Scalatra Project</name>
-        <url>http://www.scalatra.org/</url>
-      </organization>
-  
-      <licenses>
-        <license>
-          <name>BSD</name>
-          <url>http://github.com/scalatra/scalatra/raw/HEAD/LICENSE</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-  
-      <mailingLists>
-        <mailingList>
-          <name>Scalatra user group</name>
-          <archive>http://groups.google.com/group/scalatra-user</archive>
-          <post>scalatra-user@googlegroups.com</post>
-          <subscribe>scalatra-user+subscribe@googlegroups.com</subscribe>
-          <unsubscribe>scalatra-user+unsubscribe@googlegroups.com</unsubscribe>
-        </mailingList>
-      </mailingLists>
-  
-      <scm>
-        <connection>scm:git:git://github.com/scalatra/scalatra.git</connection>
-        <developerConnection>scm:git:ssh://git@github.com:scalatra/scalatra.git</developerConnection>
-        <url>http://github.com/scalatra/scalatra</url>
-      </scm>
-  
-      <distributionManagement>
-        <repository>
-          <id>sonatype-nexus-staging</id>
-          <name>Nexus Release Repository</name>
-          <url>http://oss.sonatype.org/service/local/staging/deploy/maven2</url>
-        </repository>
-        <snapshotRepository>
-          <id>sonatype-nexus-snapshots</id>
-          <name>Sonatype Nexus Snapshots</name>
-          <url>http://oss.sonatype.org/content/repositories/snapshots</url>
-        </snapshotRepository>
-      </distributionManagement>  
-  
-      <developers>
-        <developer>
-          <id>riffraff</id>
-          <name>Gabriele Renzi</name>
-          <url>http://www.riffraff.info</url>
-        </developer>
-        <developer>
-          <id>alandipert</id>
-          <name>Alan Dipert</name>
-          <url>http://alan.dipert.org</url>
-        </developer>
-        <developer>
-          <id>rossabaker</id>
-          <name>Ross A. Baker</name>
-          <url>http://www.rossabaker.com/</url>
-        </developer>
-        <developer>
-          <id>chirino</id>
-          <name>Hiram Chirino</name>
-          <url>http://hiramchirino.com/blog/</url>
-        </developer>
-      </developers>
-  
-      <modules>
-        <module>core</module>
-        <module>fileupload</module>
-        <module>scalate</module>
-        <module>example</module>
-      </modules>
-  
-      <build>
-        <plugins>
-          <plugin>
-            <groupId>org.scala-tools</groupId>
-            <artifactId>maven-scala-plugin</artifactId>
-            <version>2.14</version>
-            <executions>
-              <execution>
-                <goals>
-                  <goal>compile</goal>
-                  <goal>testCompile</goal>
-                </goals>
-              </execution>
-              <execution>
-                <id>doc</id>
-                <phase>process-classes</phase>
-                <goals>
-                  <goal>doc</goal>
-                </goals>
-                <configuration>
-                  <reportOutputDirectory>${{project.build.directory}}</reportOutputDirectory>
-                  <outputDirectory>apidocs</outputDirectory>
-                </configuration>                
-              </execution>                    
-            </executions>
-            <configuration>
-              <scaladocClassName>scala.tools.nsc.ScalaDoc</scaladocClassName>
-              <jvmArgs>
-                <jvmArg>-Xmx1024m</jvmArg>
-              </jvmArgs>
-              <args>
-                <arg>-deprecation</arg>
-              </args>
-              <scalaVersion>${{scala-version}}</scalaVersion>
-            </configuration>
-          </plugin>
+    </organization>
+    <licenses>
+      <license>
+        <name>BSD</name>
+        <url>http://github.com/scalatra/scalatra/raw/HEAD/LICENSE</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <mailingLists>
+      <mailingList>
+        <name>Scalatra user group</name>
+        <archive>http://groups.google.com/group/scalatra-user</archive>
+        <post>scalatra-user@googlegroups.com</post>
+        <subscribe>scalatra-user+subscribe@googlegroups.com</subscribe>
+        <unsubscribe>scalatra-user+unsubscribe@googlegroups.com</unsubscribe>
+      </mailingList>
+    </mailingLists>
+    <scm>
+      <connection>scm:git:git://github.com/scalatra/scalatra.git</connection>
+      <url>http://github.com/scalatra/scalatra</url>
+    </scm>
+    <developers>
+      <developer>
+        <id>riffraff</id>
+        <name>Gabriele Renzi</name>
+        <url>http://www.riffraff.info</url>
+      </developer>
+      <developer>
+        <id>alandipert</id>
+        <name>Alan Dipert</name>
+        <url>http://alan.dipert.org</url>
+      </developer>
+      <developer>
+        <id>rossabaker</id>
+        <name>Ross A. Baker</name>
+        <url>http://www.rossabaker.com/</url>
+      </developer>
+      <developer>
+        <id>chirino</id>
+        <name>Hiram Chirino</name>
+        <url>http://hiramchirino.com/blog/</url>
+      </developer>
+    </developers>
+  )
 
-          <!-- We want to sign the artifact, the POM, and all attached artifacts -->
-          <plugin>
-            <groupId>org.apache.maven.plugins</groupId>
-            <artifactId>maven-gpg-plugin</artifactId>
-            <executions>
-              <execution>
-                <id>sign-artifacts</id>
-                <phase>verify</phase>
-                <goals>
-                  <goal>sign</goal>
-                </goals>
-              </execution>
-            </executions>
-          </plugin>
-
-          <!-- attach a source jar -->
-          <plugin>
-            <artifactId>maven-source-plugin</artifactId>
-            <executions>
-              <execution>
-                <id>attach-sources</id>
-                <goals>
-                  <goal>jar</goal>
-                </goals>
-              </execution>
-            </executions>
-          </plugin>
-
-          <!-- attach a javadoc jar -->
-          <plugin>
-            <groupId>org.apache.maven.plugins</groupId>
-            <artifactId>maven-javadoc-plugin</artifactId>
-            <version>2.6</version>
-            <configuration>
-              <encoding>${{project.build.sourceEncoding}}</encoding>
-            </configuration>
-            <executions>
-              <execution>
-                <id>attach-javadocs</id>
-                <goals>
-                  <goal>jar</goal>
-                </goals>
-              </execution>
-            </executions>
-          </plugin>
-
-        </plugins>
-      </build>
-  
-      <profiles>
-        <!-- poms deployed to maven central CANNOT have a repositories
-             section defined.  This download profile lets you 
-             download dependencies other repos during development time. -->
-        <profile>
-          <id>download</id>
-          {pom \ "repositories"}
-  
-          <pluginRepositories>
-            <pluginRepository>
-              <id>scalatools.releases</id>
-              <url>http://scala-tools.org/repo-releases</url>
-              <snapshots><enabled>false</enabled></snapshots>
-              <releases><enabled>true</enabled></releases>
-            </pluginRepository>
-            <pluginRepository>
-              <id>scalatools.snapshots</id>
-              <url>http://scala-tools.org/repo-snapshots</url>
-              <snapshots><enabled>true</enabled></snapshots>
-              <releases><enabled>false</enabled></releases>
-            </pluginRepository>
-          </pluginRepositories>
-        </profile>
-      </profiles>
-    </project>
-
-  def crossScalaName(name: String) = name+"_"+crossScalaVersionString
-
-  def sortDependencies(deps: Seq[Node]) = {
-    def sortKey(dep: Node) = (dep \ "artifactId")(0).text
-    stableSort(deps, {(x: Node, y:Node) => sortKey(x) < sortKey(y)})
-  }
+  override def pomPostProcess(pom: Node) = 
+    pom match { 
+      case Elem(prefix, label, attr, scope, c @ _*) =>
+        val children = c flatMap {
+          case Elem(_, "repositories", _, _, repos @ _*) =>
+            <profiles>
+              <!-- poms deployed to maven central CANNOT have a repositories
+                   section defined.  This download profile lets you 
+                   download dependencies other repos during development time. -->
+              <profile>
+                <id>download</id>
+                {repos}
+              </profile>
+            </profiles>
+          case Elem(_, "dependencies", _, _, _ @ _*) =>
+            // In SBT, parent projects depend on their children.  They should
+            // not in Maven.
+            None
+          case x => x
+        }
+        Elem(prefix, label, attr, scope, children : _*)
+    }
 
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
-  
 }
