7d973e45a7871b91ed130a56940ab3a4f504b432
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-10 13:14:19 -0400

    Look for a Scaml template if no route matches.

--- website/src/main/scala/org/scalatra/www/WebsiteFilter.scala
+++ website/src/main/scala/org/scalatra/www/WebsiteFilter.scala
@@ -1,14 +1,26 @@
 package org.scalatra
 package www
 
+import java.net.URL
 import scalate.ScalateSupport
 
 class WebsiteFilter 
   extends ScalatraFilter 
   with ScalateSupport
 {
-  get("/") {
-    contentType = "text/html"
-    templateEngine.layout("/index.scaml")
+  notFound {
+    // If no route matches, then try to render a Scaml template
+    val templateBase = requestPath match {
+      case s if s.endsWith("/") => s + "index"
+      case s => s
+    }
+    val templatePath = templateBase + ".scaml"
+    servletContext.getResource(templatePath) match {
+      case url: URL => 
+        contentType = "text/html"
+        templateEngine.layout(templatePath)
+      case _ => 
+        filterChain.doFilter(request, response)
+    } 
   }
 }
