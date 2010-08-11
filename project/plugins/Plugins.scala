3500c86bef0c423aec346223acac88b21f7f9be0
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-11 13:02:49 -0400

    Use sbt-gpg-plugin-0.1.0.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info)
 {
-  val gpgPlugin = "com.rossabaker" % "sbt-gpg-plugin" % "0.1.0-SNAPSHOT"
+  val gpgPlugin = "com.rossabaker" % "sbt-gpg-plugin" % "0.1.0"
 }
