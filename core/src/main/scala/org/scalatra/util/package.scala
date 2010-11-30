a8fa47b82b2bfd294d49b2f1d1b0eb3d7b7ea788
Ross A. Baker <baker@alumni.indiana.edu>
2010-11-29 21:04:04 -0500

    using invokes a block with a closeable resource.

---
+++ core/src/main/scala/org/scalatra/util/package.scala
@@ -0,0 +1,21 @@
+package org.scalatra
+
+package object util {
+  /**
+   * Executes a block with a closeable resource, and closes it after the block runs
+   *
+   * @tparam A the return type of the block
+   * @tparam B the closeable resource type
+   * @param closeable the closeable resource
+   * @param f the block
+   */
+  def using[A, B <: { def close(): Unit }](closeable: B)(f: B => A) {
+    try {
+      f(closeable)
+    }
+    finally {
+      if (closeable != null)
+        closeable.close()
+    }
+  }
+}
