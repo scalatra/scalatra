6a480a43152722233662e7f547b77044194c344f
Ross A. Baker <baker@alumni.indiana.edu>
2011-03-01 10:32:59 -0500

    Clean up unused plugins.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,7 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info)
 {
-//  val gpgPlugin = "com.rossabaker" % "sbt-gpg-plugin" % "0.1.1"
   val scalatePlugin = "org.fusesource.scalate" % "sbt-scalate-plugin" % "1.4.1"
-  val openpgp = "org.bouncycastle" % "bcpg-jdk16" % "1.45"
 }
