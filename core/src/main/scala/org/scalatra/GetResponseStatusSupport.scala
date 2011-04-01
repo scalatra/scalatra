1b644e459e4e83d6211e8fb589c9d7a0d7419c44
Ross A. Baker <baker@alumni.indiana.edu>
2011-04-01 00:02:33 -0400

    Capture response status on redirect and sendError.

--- core/src/main/scala/org/scalatra/GetResponseStatusSupport.scala
+++ core/src/main/scala/org/scalatra/GetResponseStatusSupport.scala
@@ -3,6 +3,10 @@ package org.scalatra
 import javax.servlet.http.{HttpServletRequest, HttpServletResponseWrapper, HttpServletResponse}
 import java.util.concurrent.atomic.AtomicInteger
 
+/**
+ * Mixin to ScalatraKernel that allows the retrieval of the HttpStatus.
+ * The response.getStatus() method was not added until Servlet 3.0.
+ */
 trait GetResponseStatusSupport extends Handler { self: ScalatraKernel =>
 
   abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
@@ -20,7 +24,23 @@ trait GetResponseStatusSupport extends Handler { self: ScalatraKernel =>
       _status.set(sc)
       resp setStatus sc
     }
+
+    override def sendRedirect(location: String) {
+      _status.set(HttpServletResponse.SC_MOVED_TEMPORARILY)
+      resp.sendRedirect(location)
+    }
+
+    override def sendError(sc: Int) {
+      _status.set(sc)
+      resp.sendError(sc)
+    }
+
+    override def sendError(sc: Int, msg: String) {
+      _status.set(sc)
+      resp.sendError(sc, msg)
+    }
   }
 
+  // TODO will fail if there is another response wrapper of a different type
   def status = response.asInstanceOf[ScalatraGetStatusServletResponseWrapper].getStatus
 }
---
+++ core/src/test/scala/org/scalatra/GetResponseStatusSupportTest.scala
@@ -0,0 +1,66 @@
+package org.scalatra
+
+import org.scalatest.matchers.ShouldMatchers
+import test.scalatest.ScalatraFunSuite
+
+class GetResponseStatusSupportTestServlet extends ScalatraServlet with GetResponseStatusSupport {
+  before {
+    session // Establish a session before we commit the response
+  }
+
+  after {
+    session("status") = status.toString
+  }
+
+  get("/status/:status") {
+    response.setStatus(params("status").toInt)
+    status.toString
+  }
+
+  get("/redirect") {
+    response.sendRedirect("/")
+  }
+
+  get("/session-status") {
+    session.getOrElse("status", "none")
+  }
+
+  get("/send-error/:status") {
+    response.sendError(params("status").toInt)
+  }
+
+  get("/send-error/:status/:msg") {
+    response.sendError(params("status").toInt, params("msg"))
+  }
+}
+
+class GetResponseStatusSupportTest extends ScalatraFunSuite with ShouldMatchers {
+  addServlet(classOf[GetResponseStatusSupportTestServlet], "/*")
+
+  test("remember status after setStatus") {
+    get("/status/404") {
+      body should equal ("404")
+    }
+  }
+
+  test("remembers status after sendRedirect") {
+    session {
+      get("/redirect") {}
+      get("/session-status") { body should equal ("302") }
+    }
+  }
+
+  test("remembers status after sendError without a message") {
+    session {
+      get("/send-error/500") {}
+      get("/session-status") { body should equal ("500") }
+    }
+  }
+
+  test("remembers status after sendError with a message") {
+    session {
+      get("/send-error/504/Gateway%20Timeout") {}
+      get("/session-status") { body should equal ("504") }
+    }
+  }
+}
