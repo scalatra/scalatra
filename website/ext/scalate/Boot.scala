66009b50b3c1792f76d3684f5a6f29881f6600f0
Ross A. Baker <baker@alumni.indiana.edu>
2011-04-07 13:08:46 -0400

    Pygmentize the website.
    Install Python pygments to make the website project look pretty.  Or
    prettier, anyway.

--- website/ext/scalate/Boot.scala
+++ website/ext/scalate/Boot.scala
@@ -1,9 +1,16 @@
 package scalate
 
+import java.util.regex.Matcher
+import org.fusesource.scalamd.{MacroDefinition, Markdown}
 import org.fusesource.scalate._
+import org.fusesource.scalate.wikitext.Pygmentize
 
 class Boot(engine: TemplateEngine) {
   def run: Unit = {
+    def pygmentize(m: Matcher): String = Pygmentize.pygmentize(m.group(2), m.group(1))
+
+    Markdown.macros :::= List(MacroDefinition("""\{pygmentize::(.*?)\}(.*?)\{pygmentize\}""", "s", pygmentize, true))
+
     for (ssp <- engine.filter("ssp"); md <- engine.filter("markdown")) {
       engine.pipelines += "ssp.md"-> List(ssp, md)
     }
