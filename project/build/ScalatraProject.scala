44c5982e2f83137671ea47969d91f2d6390faaec
Ross A. Baker <baker@alumni.indiana.edu>
2011-06-02 15:19:06 -0400

    Fix test dependencies for Scala 2.8.x.

--- project/build/ScalatraProject.scala
+++ project/build/ScalatraProject.scala
@@ -187,30 +187,25 @@ class ScalatraProject(info: ProjectInfo)
   })
 
   lazy val scalatest = project("scalatest", "scalatra-scalatest", new DefaultProject(_) with ScalatraSubproject {
-    val artifactId = "scalatest_" + (buildScalaVersion match {
-      case "2.9.0-1" => "2.9.0"
-      case x => x
-    })
-    val ver = buildScalaVersion match {
+    val scalatest = "org.scalatest" % (buildScalaVersion match {
+      case x if (x startsWith "2.9.") => "scalatest_2.9.0"
+      case x if (x startsWith "2.8") => "scalatest"
+    }) % (buildScalaVersion match {
       case x if (x startsWith "2.9.") => "1.4.1"
       case x if (x startsWith "2.8.") => "1.3"
-    }
-    val scalatest = "org.scalatest" % artifactId % ver % "compile"
+    }) % "compile"
     val junit = "junit" % "junit" % "4.8.1" % "compile"
     val description = "ScalaTest support for the Scalatra test framework"
   }, scalatraTest)
 
   lazy val specs = project("specs", "scalatra-specs", new DefaultProject(_) with ScalatraSubproject {
-    val artifactId = "specs_" + (buildScalaVersion match {
-      case "2.9.0-1" => "2.9.0"
-      case x => x
-    })
-    val ver = buildScalaVersion match {
-      case x if (x startsWith "2.9.") => "1.6.8"
-      case "2.8.1" => "1.6.7.2"
+    val scalatest = "org.scala-tools.testing" % (buildScalaVersion match {
+      case "2.9.0-1" => "specs_2.9.0"
+      case x => "specs_"+x
+    }) % (buildScalaVersion match {
       case "2.8.0" => "1.6.5"
-    }
-    val specs = "org.scala-tools.testing" % artifactId % ver % "compile"
+      case x => "1.6.8"
+    }) % "compile"
     val description = "Specs support for the Scalatra test framework"
   }, scalatraTest)
 
