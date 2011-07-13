35eaec530103b46c620cad4bf65d646caefc6a9d
Ross A. Baker <baker@alumni.indiana.edu>
2011-07-12 22:54:36 -0400

    Provide working example of a Specs2 spec. [fixes #65]
    - Tester blocks (e.g., get, post) now return values instead of Unit.
    - Override `map` instead of `is` in ScalatraSpec.

--- specs2/src/main/scala/org/scalatra/specs2/ScalatraSpec.scala
+++ specs2/src/main/scala/org/scalatra/specs2/ScalatraSpec.scala
@@ -1,26 +1,19 @@
 package org.scalatra.specs2
 
 import org.scalatra.test.ScalatraTests
-import org.specs2._
 import org.eclipse.jetty.testing.ServletTester
-import specification.{Step, Fragments}
+import org.specs2.execute.Result
+import org.specs2.specification.{Around, BeforeAfter, Step, Fragments}
+import org.specs2.Specification
 
 /**
  * A Specification that starts the tester before the specification and stops it afterward.
  *
  * This is a spec of the immutable variation of the specs2 framework.
- * All documentation for specs2 still applies (except for the fact that you don't have to close a spec with the end fragment).
+ * All documentation for specs2 still applies.
  */
-trait ScalatraSpec extends Specification with ScalatraTests { self: Specification =>
-
+trait ScalatraSpec extends Specification with ScalatraTests {
   lazy val tester = new ServletTester
-  
-  def startStep = Step(start())
-  def stopStep = Step(stop())
 
-  abstract override def is = {
-    startStep     ^
-    super.is      ^
-    stopStep      ^ end
-  }
+  override def map(fs: =>Fragments) = Step(start()) ^ fs ^ Step(stop())
 }
