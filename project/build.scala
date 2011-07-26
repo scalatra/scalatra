1d8b7ab77efce1d6d1d8fc1952dba91e1c27e001
Ross A. Baker <baker@alumni.indiana.edu>
2011-07-25 20:39:06 -0400

    List 2.9.0-1 as first crossbuild version to keep it as the default.

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object ScalatraBuild extends Build {
   val scalatraSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalatra",
     version := "2.0.0-SNAPSHOT",
-    crossScalaVersions := Seq("2.9.1.RC1", "2.9.0-1", "2.9.0"),
+    crossScalaVersions := Seq("2.9.0-1", "2.9.1.RC1", "2.9.0"),
     scalaVersion <<= (crossScalaVersions) { versions => versions.head },
     packageOptions <<= (packageOptions, name, version, organization) map {
       (opts, title, version, vendor) => 
