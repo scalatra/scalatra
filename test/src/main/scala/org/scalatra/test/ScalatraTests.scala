8d53f3ef16e29d9740d8dfdd8f1176ac751595b6
Ross A. Baker <baker@alumni.indiana.edu>
2011-07-28 22:04:45 -0400

    Support arbitrary requests in ScalatraTests. [fixes #51]

--- test/src/main/scala/org/scalatra/test/ScalatraTests.scala
+++ test/src/main/scala/org/scalatra/test/ScalatraTests.scala
@@ -42,38 +42,33 @@ trait ScalatraTests {
   private def toQueryString(params: Traversable[(String, String)]) =
     params.map(t => List(t._1, t._2).map(encode(_, "UTF-8")).mkString("=")).mkString("&")
 
-  private def httpRequest(method: String, uri: String, queryParams: Iterable[(String, String)] = Map.empty,
-                          headers: Map[String, String] = Map.empty, body: String = null) = {
-    val req = {
-      val r = new HttpTester("iso-8859-1")
-      r.setVersion("HTTP/1.0")
-      r.setMethod(method)
-      val queryString = toQueryString(queryParams)
-      r.setURI(uri + (if (queryString == "") "" else "?") + queryString)
-      r.setContent(body)
-      headers.foreach(t => r.setHeader(t._1, t._2))
-      _cookies.value foreach(c => r.setHeader("Cookie", c.toString))
-      r
-    }
-
+  def submit[A](req: HttpTester)(f: => A): A = {
     val res = new HttpTester("iso-8859-1")
     res.parse(tester.getResponses(req.generate))
     res.setContent(res.getContent match {
       case null => ""
       case content => content
     })
-    res
-  }
-
-  private def withResponse[A](r: HttpTester, f: => A) = {
-    val result = _response.withValue(r)(f)
-    if(_useSession.value && r.getHeader("Set-Cookie") != null) {
-      val setCookies = r.getHeaderValues("Set-Cookie").asInstanceOf[Enumeration[String]]
+    if (_useSession.value && res.getHeader("Set-Cookie") != null) {
+      val setCookies = res.getHeaderValues("Set-Cookie").asInstanceOf[Enumeration[String]]
       _cookies.value = setCookies flatMap { setCookie => 
         HttpCookie.parse(setCookie)
       } toSeq
     }
-    result
+    _response.withValue(res) { f }
+  }
+
+  def submit[A](method: String, uri: String, queryParams: Iterable[(String, String)] = Map.empty,
+                headers: Map[String, String] = Map.empty, body: String = null)(f: => A): A = {
+    val req = new HttpTester("iso-8859-1")
+    req.setVersion("HTTP/1.0")
+    req.setMethod(method)
+    val queryString = toQueryString(queryParams)
+    req.setURI(uri + (if (queryString == "") "" else "?") + queryString)
+    req.setContent(body)
+    headers.foreach(t => req.setHeader(t._1, t._2))
+    _cookies.value foreach(c => req.setHeader("Cookie", c.toString))
+    submit(req) { f }
   }
 
   @deprecated("use addServlet(Class, String) or addFilter(Class, String)", "2.0")
@@ -111,17 +106,17 @@ trait ScalatraTests {
   def routeFilter(filter: Class[_ <: Filter], path: String) =
     addFilter(filter, path)
 
-  def get[A](uri: String)(f: => A): A = withResponse(httpRequest("GET", uri), f)
+  def get[A](uri: String)(f: => A): A = submit("GET", uri) { f }
   def get[A](uri: String, params: Tuple2[String, String]*)(f: => A): A =
     get(uri, params, Map[String, String]())(f)
   def get[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A =
-    withResponse(httpRequest("GET", uri, params, headers), f)
+    submit("GET", uri, params, headers) { f }
 
-  def head[A](uri: String)(f: => A): A = withResponse(httpRequest("HEAD", uri), f)
+  def head[A](uri: String)(f: => A): A = submit("HEAD", uri) { f }
   def head[A](uri: String, params: Tuple2[String, String]*)(f: => A): A =
     get(uri, params, Map[String, String]())(f)
   def head[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A =
-    withResponse(httpRequest("HEAD", uri, params, headers), f)
+    submit("HEAD", uri, params, headers) { f } 
 
   def post[A](uri: String, params: Tuple2[String, String]*)(f: => A): A =
     post(uri, params)(f)
@@ -130,7 +125,7 @@ trait ScalatraTests {
   def post[A](uri: String, params: Iterable[(String,String)], headers: Map[String, String])(f: => A): A =
     post(uri, toQueryString(params), Map("Content-Type" -> "application/x-www-form-urlencoded; charset=utf-8") ++ headers)(f)
   def post[A](uri: String, body: String = "", headers: Map[String, String] = Map.empty)(f: => A): A =
-    withResponse(httpRequest("POST", uri, Seq.empty, headers, body), f)
+    submit("POST", uri, Seq.empty, headers, body) { f } 
   // @todo support POST multipart/form-data for file uploads
 
   def put[A](uri: String, params: Tuple2[String, String]*)(f: => A): A =
@@ -140,24 +135,20 @@ trait ScalatraTests {
   def put[A](uri: String, params: Iterable[(String,String)], headers: Map[String, String])(f: => A): A =
     put(uri, toQueryString(params), Map("Content-Type" -> "application/x-www-form-urlencoded; charset=utf-8") ++ headers)(f)
   def put[A](uri: String, body: String = "", headers: Map[String, String] = Map.empty)(f: => A) =
-    withResponse(httpRequest("PUT", uri, Seq.empty, headers, body), f)
+    submit("PUT", uri, Seq.empty, headers, body) { f }
   // @todo support PUT multipart/form-data for file uploads
   
-  def delete[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = {
-    withResponse(httpRequest("DELETE", uri, params, headers), f)
-  }
+  def delete[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = 
+    submit("DELETE", uri, params, headers) { f }
 
-  def options[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = {
-    withResponse(httpRequest("OPTIONS", uri, params, headers), f)
-  }
+  def options[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = 
+    submit("OPTIONS", uri, params, headers) { f }
 
-  def trace[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = {
-    withResponse(httpRequest("TRACE", uri, params, headers), f)
-  }
+  def trace[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = 
+    submit("TRACE", uri, params, headers) { f }
 
-  def connect[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = {
-    withResponse(httpRequest("CONNECT", uri, params, headers), f)
-  }
+  def connect[A](uri: String, params: Iterable[(String, String)] = Seq.empty, headers: Map[String, String] = Map.empty)(f: => A): A = 
+    submit("CONNECT", uri, params, headers) { f }
 
   def session[A](f: => A): A = {
     _cookies.withValue(Nil) {
