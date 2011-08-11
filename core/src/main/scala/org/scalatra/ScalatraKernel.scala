1cda8a7f827760feb3b7200306e53988382ddd97
Ross A. Baker <baker@alumni.indiana.edu>
2011-08-11 02:18:37 -0400

    Remove kernelName, used only by an erstwhile hack.

--- core/src/main/scala/org/scalatra/ScalatraKernel.scala
+++ core/src/main/scala/org/scalatra/ScalatraKernel.scala
@@ -451,9 +451,4 @@ trait ScalatraKernel extends Handler with Initializable
 
   def environment: String = System.getProperty(EnvironmentKey, initParameter(EnvironmentKey).getOrElse("development"))
   def isDevelopmentMode = environment.toLowerCase.startsWith("dev")
-
-  /**
-   * Uniquely identifies this ScalatraKernel inside the webapp.
-   */
-  def kernelName: String
 }
