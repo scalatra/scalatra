scalatra/scalatra

commit f01cef492b613ba09bc9c42fbabb8987a48aadba

[breaking] Refactor error handling; add isScalateErrorPageEnabled. [fixes #93]
Replace this: `error { dealWith(caughtThrowable) }`
with this: `error { case e => dealWith(caughtThrowable) }`

scalate/src/test/scala/org/scalatra/scalate/ScalateSupportSpec.scala
@@ -1,0 +1,19 @@
+package org.scalatra
+package scalate
+
+import specs2.ScalatraSpec
+
+class ScalateSupportSpec extends ScalatraSpec { def is =
+  "ScalateSupport should"                       ^
+    "render uncaught errors with 500.scaml"     ! e1
+
+  addServlet(new ScalatraServlet with ScalateSupport {
+    get("/barf") {
+      throw new RuntimeException
+    }
+  }, "/*")
+
+  def e1 = get("/barf") {
+    body must contain ("""id="scalate-error"""")
+  }
+}

README.markdown
@@ -377,10 +377,13 @@ handle the request.
 
 ### Error
 
-The `error` handler is invoked any time an exception is raised from a route block or a filter.  The throwable can be obtained from the `caughtThrowable` instance variable.  This variable is not defined outside the `error` block.
+The `error` handler is invoked any time an exception is raised from a route
+block or a before filter.  The error handler is a pattern matcher prepended
+to the previous error handler.  The default behavior is to rethrow the 
+exception.
 
-    error {
-      log.error(caughtThrowable)
+    error { case e =>
+      log.error(e)
       redirect("http://www.sadtrombone.com/")
     }
 
@@ -674,6 +677,15 @@ Another difference is that ScalatraFilter matches routes relative to the WAR's c
 2. `halt(Int)` and `halt(Int, String)` no longer call `response.sendError`.
    If you depend on this behavior for routing to web.xml error pages, call 
    it explicitly and then call `halt()`, or override `halt`.
+
+3. `caughtThrowable` has been removed.  The error block is now a partial
+   function that pattern matches the exception.  Replace this:
+
+       error { caughtThrowable.printStackTrace() }
+
+   With this:
+
+       error { case e => e.printStackTrace() }
 
 ### scalatra-2.0.0.M3 to scalatra-2.0.0.M4
 

core/src/main/scala/org/scalatra/CoreDsl.scala
@@ -116,16 +116,18 @@ trait CoreDsl {
   def methodNotAllowed(block: Set[HttpMethod] => Any): Unit
 
   /**
-   * Defines a block to run if an exception is thrown.  The exception is
-   * passed to the block.
+   * Defines an error handler for exceptions thrown in either the before
+   * block or a route action.
+   *
+   * If the error handler does not match, the result falls through to the
+   * previously defined error handler.  The default error handler simply
+   * rethrows the exception.
+   *
+   * The error handler is run before the after filters, and the result is
+   * rendered like a standard response.  It is the error handler's
+   * responsibility to set any appropriate status code.
    */
-  def error(block: => Any): Unit
-
-  /**
-   * The throwable currently handled by the error block.  Invalid outside
-   * an error block.
-   */
-  def caughtThrowable: Throwable
+  def error(handler: ErrorHandler): Unit
 
   /**
    * The Scalatra DSL core methods take a list of [[org.scalatra.RouteMatcher]]

core/src/main/scala/org/scalatra/ScalatraKernel.scala
@@ -109,7 +109,8 @@ trait ScalatraKernel extends Handler with CoreDsl with Initializable
             actionResult orElse matchOtherMethods() getOrElse doNotFound()
           }
           catch {
-            case e => handleError(e)
+            case e: HaltException => renderHaltException(e)
+            case e => errorHandler(e)
           }
           finally {
             runFilters(routes.afterFilters)
@@ -169,25 +170,8 @@ trait ScalatraKernel extends Handler with CoreDsl with Initializable
     if (allow.isEmpty) None else Some(doMethodNotAllowed(allow))
   }
 
-  protected def handleError(e: Throwable): Any = {
-    (renderError orElse defaultRenderError).apply(e)
-  }
-
-  protected def renderError : PartialFunction[Throwable, Any] = defaultRenderError
-
-  protected final def defaultRenderError : PartialFunction[Throwable, Any] = {
-    case e: HaltException => renderHaltException(e)
-    case e => {
-      status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
-      _caughtThrowable.withValue(e) { errorHandler() }
-    }
-  }
-
-  protected var errorHandler: Action = { () => throw caughtThrowable }
-  def error(fun: => Any) = errorHandler = { () => fun }
-
-  private val _caughtThrowable = new DynamicVariable[Throwable](null)
-  def caughtThrowable = _caughtThrowable.value
+  protected var errorHandler: ErrorHandler = { case t => throw t }
+  def error(handler: ErrorHandler) = errorHandler = handler orElse errorHandler
 
   protected def renderResponse(actionResult: Any) {
     if (contentType == null)

core/src/main/scala/org/scalatra/package.scala
@@ -19,4 +19,6 @@ package object scalatra {
   type CSRFTokenSupport = CsrfTokenSupport
 
   type RouteMatcher = () => Option[ScalatraKernel.MultiParams]
+
+  type ErrorHandler = PartialFunction[Throwable, Any]
 }

core/src/test/scala/org/scalatra/ErrorHandlerTest.scala
@@ -2,27 +2,52 @@ package org.scalatra
 
 import test.scalatest.ScalatraFunSuite
 
-class ErrorHandlerTestServlet extends ScalatraServlet {
-  get("/") {
-    throw new RuntimeException
-  }
-
-  error {
-    "handled " + caughtThrowable.getClass.getName
-  }
-}
-
 class ErrorHandlerTest extends ScalatraFunSuite {
-  addServlet(classOf[ErrorHandlerTestServlet], "/*")
+  trait TestException extends RuntimeException
+  case class Exception1() extends TestException
+  case class Exception2() extends TestException
+
+  class BaseServlet extends ScalatraServlet {
+    get("/1") { 
+      status(418)
+      throw new Exception1 
+    } 
+    get("/uncaught") { throw new RuntimeException }
+    error { case e: TestException => "base" }
+  }
+
+  class ChildServlet extends BaseServlet {
+    get("/2") { throw new Exception2 }
+    error { case e: Exception2 => "child" }
+  }
+
+  addServlet(new BaseServlet, "/base/*")
+  addServlet(new ChildServlet, "/child/*")
 
   test("result of error handler should be rendered") {
-    get("/") {
-      body should equal ("handled java.lang.RuntimeException")
+    get("/base/1") {
+      body should equal ("base")
     }
   }
 
-  test("response status should be set to 500 on error") {
-    get("/") {
+  test("error handlers are composable") {
+    get("/child/2") {
+      body should equal ("child")
+    }
+
+    get("/child/1") {
+      body should equal ("base")
+    }
+  }
+
+  test("response status should not be set on error") {
+    get("/base/1") {
+      status should equal (418)
+    }
+  }
+
+  test("rethrows uncaught exceptions") {
+    get("/base/uncaught") {
       status should equal (500)
     }
   }

notes/2.0.0-SNAPSHOT.markdown
@@ -3,6 +3,7 @@
 * Send 405 and Allow header if other methods match. [(GH-50)](http://github.com/scalatra/scalatra/issues/50) 
 * Support bodies and headers in halt. [(GH-79)](http://github.com/scalatra/scalatra/issues/79) 
 * Support PATCH requests. [(GH-51)](http://github.com/scalatra/scalatra/issues/51) 
+* Error handler is now a pattern matcher. [(GH-93)](http://github.com/scalatra/scalatra/issues/93) 
 
 ## scalatra-anti-xml
 * New module adds support for rendering Elem responses. [(GH-75)](http://github.com/scalatra/scalatra/issues/75)

scalate/src/main/scala/org/scalatra/scalate/ScalateSupport.scala
@@ -43,18 +43,31 @@ trait ScalateSupport extends ScalatraKernel {
   def renderTemplate(path: String, attributes: (String, Any)*) = 
     createRenderContext.render(path, Map(attributes : _*))
 
-  override protected def handleError(e: Throwable) =
+  /**
+   * Flag whether the Scalate error page is enabled.  If true, uncaught 
+   * exceptions will be caught and rendered by the Scalate error page.
+   *
+   * The default is true.
+   */
+  protected def isScalateErrorPageEnabled = true 
+
+  abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
     try {
-      super.handleError(e)
+      super.handle(request, response)
     }
     catch {
-      case e => renderErrorPage(e)
+      case e if isScalateErrorPageEnabled => renderScalateErrorPage(req, res, e)
+      case e => throw e
     }
+  }
 
-  protected def renderErrorPage(e: Throwable) = {
-    contentType = "text/html"
+  // Hack: Have to pass it the request and response, because we're outside the 
+  // scope of the super handler.
+  private def renderScalateErrorPage(req: HttpServletRequest, resp: HttpServletResponse, e: Throwable) = { 
+    resp.setContentType("text/html")
     val errorPage = templateEngine.load("/WEB-INF/scalate/errors/500.scaml")
-    val renderContext = createRenderContext
+    val renderContext = 
+      new ServletRenderContext(templateEngine, req, resp, servletContext)
     renderContext.setAttribute("javax.servlet.error.exception", Some(e))
     templateEngine.layout(errorPage, renderContext)
   }

commit b526ddf2818ca0d9edc7751b959974ce9cddfd07

Backfill test for regression introduced in f01cef4.

scalate/src/test/scala/org/scalatra/scalate/ScalateSupportSpec.scala
@@ -5,15 +5,24 @@ import specs2.ScalatraSpec
 
 class ScalateSupportSpec extends ScalatraSpec { def is =
   "ScalateSupport should"                       ^
-    "render uncaught errors with 500.scaml"     ! e1
+    "render uncaught errors with 500.scaml"     ! e1^
+    "not throw a NullPointerException for trivial requests" ! e2
 
   addServlet(new ScalatraServlet with ScalateSupport {
     get("/barf") {
       throw new RuntimeException
     }
+
+    get("/happy-happy") {
+      "puppy dogs"
+    }
   }, "/*")
 
   def e1 = get("/barf") {
     body must contain ("""id="scalate-error"""")
+  }
+
+  def e2 = get("/happy-happy") {
+    body must_== "puppy dogs"
   }
 }