6ea1205000d8aaf2e5de11904d2fe72a88bb198f
Ross A. Baker <baker@alumni.indiana.edu>
2011-08-17 12:28:03 -0400

    Fix tests broken by #109.

--- core/src/main/scala/org/scalatra/RichServletContext.scala
+++ core/src/main/scala/org/scalatra/RichServletContext.scala
@@ -1,6 +1,8 @@
 package org.scalatra
 
+import java.net.{MalformedURLException, URL}
 import javax.servlet.ServletContext
+import javax.servlet.http.HttpServletRequest
 
 class RichServletContext(sc: ServletContext) extends AttributesMap {
   /*
@@ -9,4 +11,26 @@ class RichServletContext(sc: ServletContext) extends AttributesMap {
    * compiler to trust us; remove when we upgrade to Servlet 3.0.
    */
   protected def attributes = sc.asInstanceOf[Attributes]
+
+  /**
+   * Gets the resource mapped to the given path as an Option.
+   */
+  def resource(path: String): Option[URL] =
+    try {
+      Option(sc.getResource(path))
+    }
+    catch {
+      case e: MalformedURLException => println("SHIT!: " + path); throw e
+    }
+
+  /**
+   * Gets the resource mapped to the specified request path.  The
+   * request path is defined as the servlet path plus the path info, if
+   * any.
+   */
+  def resource(req: HttpServletRequest): Option[URL] = {
+    val path = req.getServletPath + (Option(req.getPathInfo) getOrElse "")
+    resource(path)
+  }
 }
+
