5b9a8e74e277be9dfc33b36ec2ba385cae6c604f
ornicar <thibault.duplessis@gmail.com>
2011-08-17 20:24:42 -0700

    Test passing params to a template

--- scalate/src/test/scala/org/scalatra/scalate/ScalateSupportSpec.scala
+++ scalate/src/test/scala/org/scalatra/scalate/ScalateSupportSpec.scala
@@ -8,8 +8,8 @@ class ScalateSupportSpec extends ScalatraSpec { def is =
   "ScalateSupport should"                                         ^
     "render uncaught errors with 500.scaml"                       ! e1^
     "not throw a NullPointerException for trivial requests"       ! e2^
-    "render a simple template"                                    ! e3
-    //"render a simple template with params"                        ! e4
+    "render a simple template"                                    ! e3^
+    "render a simple template with params"                        ! e4
 
   addServlet(new ScalatraServlet with ScalateSupport {
 
@@ -22,7 +22,11 @@ class ScalateSupportSpec extends ScalatraSpec { def is =
     }
 
     get("/simple-template") {
-      renderTemplate("simple.jade")
+      renderTemplate("/simple.jade")
+    }
+
+    get("/params") {
+      renderTemplate("/params.jade", "foo" -> "Configurable")
     }
 
   }, "/*")
@@ -39,7 +43,7 @@ class ScalateSupportSpec extends ScalatraSpec { def is =
     body must_== "<div>Simple template</div>\n"
   }
 
-  //def e4 = get("/simple-template-with-param") {
-    //body must_== "<div>Simple joy</div>\n"
-  //}
+  def e4 = get("/params") {
+    body must_== "<div>Configurable template</div>\n"
+  }
 }
