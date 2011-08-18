1a80c4b9f6ac33447cea155908bb0f89e71556ea
Ross A. Baker <baker@alumni.indiana.edu>
2011-08-18 15:47:35 -0400

    Document that util.io.copy closes the input stream.

--- core/src/main/scala/org/scalatra/util/io/package.scala
+++ core/src/main/scala/org/scalatra/util/io/package.scala
@@ -12,7 +12,8 @@ package object io {
   /**
    * Copies the input stream to the output stream.
    *
-   * @param in the input stream to read
+   * @param in the input stream to read.  The InputStream will be closed, unlike
+   * commons-io's version.
    * @param out the output stream to write
    * @param bufferSize the size of buffer to use for each read
    */
