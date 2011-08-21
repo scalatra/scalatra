8d994538ff03c4717cb025390ddb4c4cc249f8ee
ornicar <thibault.duplessis@gmail.com>
2011-08-20 21:40:43 -0700

    Start implementing rails-like url generation using regexes

--- core/src/main/scala/org/scalatra/routeMatcher.scala
+++ core/src/main/scala/org/scalatra/routeMatcher.scala
@@ -3,43 +3,43 @@ package org.scalatra
 import scala.util.matching.Regex
 import util.MultiMap
 
-trait RouteMatcher
-{
+trait RouteMatcher {
+
   def apply(): Option[ScalatraKernel.MultiParams]
 }
 
-trait ReversibleRouteMatcher
-{
+trait ReversibleRouteMatcher {
+
   def reverse(params: Map[String, String], splats: List[String]): String
 }
 
 final class SinatraRouteMatcher(path: String, requestPath: => String)
-  extends RouteMatcher with ReversibleRouteMatcher
-{
+  extends RouteMatcher with ReversibleRouteMatcher {
+
   def apply() = SinatraPathPatternParser(path)(requestPath)
 
-  def reverse(params: Map[String, String], splats: List[String]): String = {
+  def reverse(params: Map[String, String], splats: List[String]): String =
     replaceSplats(
       replaceNamedParams(
         replaceOptionalParams(path, params),
         params
       ), splats)
-  }
 
   private def replaceOptionalParams(slug: String, params: Map[String, String]): String =
-    """[\./]\?:[^/?#\.]+\?""".r replaceAllIn (path, s => {
+    """[\./]\?:[^/?#\.]+\?""".r replaceAllIn (path, s =>
       params.get(s.matched slice (3, s.matched.size - 1)) match {
         case Some(value) => s.matched.head + value
         case None => ""
-      }
-    })
+      })
 
   private def replaceNamedParams(slug: String, params: Map[String, String]): String =
-    """:[^/?#\.]+""".r replaceAllIn (slug, s =>
-      params.get(s.matched.tail) match {
+    """:[^/?#\.]+""".r replaceAllIn (slug, s => {
+      val param = s.matched.tail
+      params.get(param) match {
         case Some(value) => value
-        case None => throw new Exception("The url \"%s\" requires param \"%s\"" format (path, s))
-      })
+        case None => throw new Exception(
+          "The url \"%s\" requires param \"%s\"" format (path, param))
+    }})
 
   private def replaceSplats(slug: String, splats: List[String]): String =
     splats match {
@@ -50,17 +50,41 @@ final class SinatraRouteMatcher(path: String, requestPath: => String)
   override def toString = path
 }
 
+final class RailsRouteMatcher(path: String, requestPath: => String)
+  extends RouteMatcher with ReversibleRouteMatcher {
+
+  def apply() = RailsPathPatternParser(path)(requestPath)
+
+  def reverse(params: Map[String, String], splats: List[String]): String =
+    dynamicSegments(optionalSegments(path, params), params)
+
+  private def optionalSegments(slug: String, params: Map[String, String]): String =
+    """([^\\])\(([^\)]*[^\\])\)""".r replaceAllIn (slug, s => s.group(1) +
+      (try dynamicSegments(s.group(2), params) catch { case e: Exception => "" })
+    )
+
+  private def dynamicSegments(slug: String, params: Map[String, String]): String =
+    """(.?)(:([a-z_][^/?#\.]+))""".r replaceAllIn (slug, s => {
+      if (s.group(1) == "\\") s.group(2)
+      else params.get(s.group(3)) match {
+        case Some(value) => s.group(1) + value
+        case None => throw new Exception(
+          "The url \"%s\" requires param \"%s\"" format (path, s.group(3))
+        )
+    }})
+}
+
 final class PathPatternRouteMatcher(pattern: PathPattern, requestPath: => String)
-  extends RouteMatcher
-{
+  extends RouteMatcher {
+
   def apply() = pattern(requestPath)
 
   override def toString = pattern.regex.toString
 }
 
 final class RegexRouteMatcher(regex: Regex, requestPath: => String)
-  extends RouteMatcher
-{
+  extends RouteMatcher {
+
   def apply() = regex.findFirstMatchIn(requestPath) map { _.subgroups match {
     case Nil => MultiMap()
     case xs => Map("captures" -> xs)
@@ -69,8 +93,8 @@ final class RegexRouteMatcher(regex: Regex, requestPath: => String)
   override def toString = regex.toString
 }
 
-final class BooleanBlockRouteMatcher(block: => Boolean) extends RouteMatcher
-{
+final class BooleanBlockRouteMatcher(block: => Boolean) extends RouteMatcher {
+
   def apply() = if (block) Some(MultiMap()) else None
 
   override def toString = "[Boolean Guard]"
