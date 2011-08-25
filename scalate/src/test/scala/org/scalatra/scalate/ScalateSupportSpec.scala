625c8843e9c39552e7ef2cf2e2c6dad4e0225934
maiha <maiha@wota.jp>
2011-08-25 16:40:56 +0900

    Add syntax sugars for renderTemplate like scaml,ssp,jade,mustache.

--- scalate/src/test/scala/org/scalatra/scalate/ScalateSupportSpec.scala
+++ scalate/src/test/scala/org/scalatra/scalate/ScalateSupportSpec.scala
@@ -13,7 +13,15 @@ class ScalateSupportSpec extends ScalatraSpec { def is =
     "render a simple template with params"                        ! e4^
     "looks for layouts in /WEB-INF/layouts"                       ! e5^
     "generate a url from a template"                              ! e6^
-    "generate a url with params from a template"                  ! e7
+    "generate a url with params from a template"                  ! e7^
+    "render a simple template via jade method"                    ! e8^
+    "render a simple template with params via jade method"        ! e9^
+    "render a simple template via scaml method"                   ! e10^
+    "render a simple template with params via scaml method"       ! e11^
+    "render a simple template via ssp method"                     ! e12^
+    "render a simple template with params via ssp method"         ! e13^
+    "render a simple template via mustache method"                ! e14^
+    "render a simple template with params via mustache method"    ! e15
 
   addServlet(new ScalatraServlet with ScalateSupport with ScalateUrlGeneratorSupport {
 
@@ -31,6 +39,38 @@ class ScalateSupportSpec extends ScalatraSpec { def is =
 
     get("/params") {
       renderTemplate("/params.jade", "foo" -> "Configurable")
+    }
+
+    get("/jade-template") {
+      jade("simple")
+    }
+
+    get("/jade-params") {
+      jade("params", "foo" -> "Configurable")
+    }
+
+    get("/scaml-template") {
+      scaml("simple")
+    }
+
+    get("/scaml-params") {
+      scaml("params", "foo" -> "Configurable")
+    }
+
+    get("/ssp-template") {
+      ssp("simple")
+    }
+
+    get("/ssp-params") {
+      ssp("params", "foo" -> "Configurable")
+    }
+
+    get("/mustache-template") {
+      mustache("simple")
+    }
+
+    get("/mustache-params") {
+      mustache("params", "foo" -> "Configurable")
     }
 
     get("/layout-strategy") {
@@ -84,5 +124,37 @@ class ScalateSupportSpec extends ScalatraSpec { def is =
 
   def e7 = get("/url-generation-with-params/jedi/vs/sith") {
     body must_== "/url-generation-with-params/jedi/vs/sith\n"
+  }
+
+  def e8 = get("/jade-template") {
+    body must_== "<div>Jade template</div>\n"
+  }
+
+  def e9 = get("/jade-params") {
+    body must_== "<div>Configurable template</div>\n"
+  }
+
+  def e10 = get("/scaml-template") {
+    body must_== "<div>Scaml template</div>\n"
+  }
+
+  def e11 = get("/scaml-params") {
+    body must_== "<div>Configurable template</div>\n"
+  }
+
+  def e12 = get("/ssp-template") {
+    body must_== "<div>SSP template</div>\n"
+  }
+
+  def e13 = get("/ssp-params") {
+    body must_== "<div>Configurable template</div>\n"
+  }
+
+  def e14 = get("/mustache-template") {
+    body must_== "<div>Mustache template</div>\n"
+  }
+
+  def e15 = get("/mustache-params") {
+    body must_== "<div>Configurable template</div>\n"
   }
 }
