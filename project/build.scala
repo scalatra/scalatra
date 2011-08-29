a0a26bddeeeddb190dac47d661f6929d7579fdef
Ross A. Baker <baker@alumni.indiana.edu>
2011-08-29 01:52:34 -0400

    Revert to 2.0.0-SNAPSHOT.

--- project/build.scala
+++ project/build.scala
@@ -9,7 +9,7 @@ object ScalatraBuild extends Build {
 
   val scalatraSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.scalatra",
-    version := "2.0.0.RC1",
+    version := "2.0.0-SNAPSHOT",
     crossScalaVersions := Seq("2.9.0-1", "2.9.1.RC4", "2.9.0"),
     scalaVersion <<= (crossScalaVersions) { versions => versions.head },
     scalacOptions ++= Seq("-unchecked", "-deprecation"),
