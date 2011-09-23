95b2bce28e1cd6084d01469d95e54871233584f3
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-09-23 22:13:43 +0200

    github ninja fix, i'll be very lucky if this doesn't break anything

--- core/src/main/scala/org/scalatra/SslRequirement.scala
+++ core/src/main/scala/org/scalatra/SslRequirement.scala
@@ -6,18 +6,14 @@ import java.net.URI
 /**
  * Redirects unsecured requests to the corresponding secure URL.
  */
-trait SslRequirement extends Handler { self: ScalatraKernel =>
+trait SslRequirement extends Handler { 
   abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
-    _request.withValue(req) {
-      _response.withValue(res) {
-        if(!req.isSecure) {
-          val oldUri = new URI(req.getRequestURL.toString)
-          val url = new URI("https", oldUri.getAuthority, oldUri.getPath, oldUri.getQuery, oldUri.getFragment).toString
-          redirect(url)
-        } else {
-          super.handle(req, res)
-        }
-      }
+    if(!req.isSecure) {
+      val oldUri = new URI(req.getRequestURL.toString)
+      val url = new URI("https", oldUri.getAuthority, oldUri.getPath, oldUri.getQuery, oldUri.getFragment).toString
+      res.sendRedirect(url)
+    } else {
+      super.handle(req, res)
     }
   }
 }
