258dc283253ce611a4d9c8c6f1b225628beb9b09
Robbie Clutton <robert.clutton@gmail.com>
2011-12-23 16:25:02 +0000

    add parameters into the case class and render those

--- docs/src/main/scala/org/scalatra/docs/Documentation.scala
+++ docs/src/main/scala/org/scalatra/docs/Documentation.scala
@@ -2,13 +2,28 @@ package org.scalatra.docs
 
 import org.scalatra.{Get, HttpMethod}
 
-case class Documentation(name: String, route: String, method: HttpMethod = Get) {
+case class Documentation(name: String, route: String, description: String, method: HttpMethod = Get, requiredParams: List[Param] = List(), optionalParams: List[Param] = List()) {
+
+  implicit def list2HtmlList(list: List[Param]): ParamList = ParamList(list)
 
   def toHtml() = {
     """
     <h1>%s %s</h1>
     <h2>%s</h2>
-    """.format(method, route, name)
+    <p>%s</p>
+    <h3>Required parameters</h3>
+    %s
+    <h3>Optional parameters</h3>
+    %s
+    """.format(method, route, name, description, requiredParams.toHtml, optionalParams.toHtml)
   }
+  
+}
 
+case class Param(name: String, description: String) {
+  def toHtml() = "%s - %s".format(name, description)          
+}
+
+case class ParamList(list: List[Param]){
+    def toHtml = list.map( _.toHtml ).mkString("<ul><li>", "</li><li>", "</li></ul>")
 }
--- example/src/main/scala/org/scalatra/DocumentExample.scala
+++ example/src/main/scala/org/scalatra/DocumentExample.scala
@@ -4,7 +4,8 @@ import docs._
 
 class DocumentExample extends ScalatraServlet with DocumentationSupport {
 
-  val helloDoc = Documentation(name = "hello world", route = "/hello")
+  val helloParam = Param(name = "hello", description = "The hello parameter")
+  val helloDoc = Documentation(name = "hello world", route = "/hello", description = "The hello world documentation example", requiredParams = List(helloParam), optionalParams = List(helloParam))
 
   before(){
     contentType = "text/html"
