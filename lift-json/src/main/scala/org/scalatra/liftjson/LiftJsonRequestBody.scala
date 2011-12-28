ff1c4a9db7db7f8f2a0c2b3af8ac795448647209
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-12-28 10:23:15 +0100

    make protected formats optional to avoid clashes with other implicits

--- lift-json/src/main/scala/org/scalatra/liftjson/LiftJsonRequestBody.scala
+++ lift-json/src/main/scala/org/scalatra/liftjson/LiftJsonRequestBody.scala
@@ -12,37 +12,45 @@ object LiftJsonRequestBody {
 
   val ParsedBodyKey = "org.scalatra.liftjson.ParsedBody".intern
 }
+
+trait LiftJsonRequestBodyWithoutFormats extends ScalatraKernel with ApiFormats {
+  import LiftJsonRequestBody._
+   
+    protected def parseRequestBody(format: String, content: String) = try {
+      if (format == "json") {
+        transformRequestBody(JsonParser.parse(content))
+      } else if (format == "xml") {
+        transformRequestBody(toJson(scala.xml.XML.loadString(content)))
+      } else JString(content)
+    } catch { case _ ⇒ JNothing }
+  
+    protected def transformRequestBody(body: JValue) = body
+  
+    override protected def invoke(matchedRoute: MatchedRoute) = {
+      withRouteMultiParams(Some(matchedRoute)) {
+        val mt = request.getContentType.toOption map { _.split(";").head } getOrElse "application/x-www-form-urlencoded"
+        val fmt = mimeTypes get mt getOrElse "html"
+        if (shouldParseBody(fmt)) {
+          request(ParsedBodyKey) = parseRequestBody(fmt, request.body)
+        }
+        super.invoke(matchedRoute)
+      }
+    }
+
+    private def shouldParseBody(fmt: String) =
+      (fmt == "json" || fmt == "xml") && request.get(LiftJsonRequestBody.ParsedBodyKey).isEmpty
+  
+    def parsedBody = request.get(ParsedBodyKey) getOrElse JNothing
+}
+
 /**
  * Parses request bodies with lift json if the appropriate content type is set.
  * Be aware that it also parses XML and returns a JValue of the parsed XML.
  */
-trait LiftJsonRequestBody extends ScalatraKernel with ApiFormats {
+trait LiftJsonRequestBody extends LiftJsonRequestBodyWithoutFormats {
 
   protected implicit def jsonFormats: Formats = DefaultFormats
 
-  import LiftJsonRequestBody._
- 
-  protected def parseRequestBody(format: String, content: String) = try {
-    if (format == "json") {
-      transformRequestBody(JsonParser.parse(content))
-    } else if (format == "xml") {
-      transformRequestBody(toJson(scala.xml.XML.loadString(content)))
-    } else JString(content)
-  } catch { case _ ⇒ JNothing }
-
-  protected def transformRequestBody(body: JValue) = body
-
-  override protected def invoke(matchedRoute: MatchedRoute) = {
-    withRouteMultiParams(Some(matchedRoute)) {
-      val mt = request.getContentType.toOption map { _.split(";").head } getOrElse "application/x-www-form-urlencoded"
-      val fmt = mimeTypes get mt getOrElse "html"
-      if (fmt == "json" || fmt == "xml") {
-        request(ParsedBodyKey) = parseRequestBody(fmt, request.body)
-      }
-      super.invoke(matchedRoute)
-    }
-  }
-
-  def parsedBody = request.get(ParsedBodyKey) getOrElse JNothing
+  
 
 }
