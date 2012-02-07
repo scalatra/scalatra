4489f04b0fd8ba7cf95b6aec43e51d329c32d53e
Philip Wills <philip.wills@guardian.co.uk>
2012-02-07 17:15:39 +0000

    Add JSONP support to the JsonSupport trait.

--- lift-json/src/test/scala/org/scalatra/liftjson/JsonSupportTest.scala
+++ lift-json/src/test/scala/org/scalatra/liftjson/JsonSupportTest.scala
@@ -4,13 +4,27 @@ import org.scalatra.test.scalatest.ScalatraFunSuite
 import org.scalatra.ScalatraServlet
 
 class JsonSupportTest extends ScalatraFunSuite {
-  val servletHolder = addServlet(classOf[JsonSupportTestServlet], "/*")
-  servletHolder.setInitOrder(1) // force load on startup
+  addServlet(classOf[JsonSupportTestServlet], "/*")
+  addServlet(classOf[JsonPTestServlet], "/p/*")
 
   test("JSON support test") {
     get("/json") {
       response.mediaType should equal (Some("application/json"))
       response.body should equal ("""{"k1":"v1","k2":"v2"}""")
+    }
+  }
+
+  test("JSONP callback test with no callback name specified") {
+    get("/json", "callback" -> "function") {
+      response.mediaType should equal (Some("application/json"))
+      response.body should equal ("""{"k1":"v1","k2":"v2"}""")
+    }
+  }
+
+  test("JSONP callback test with callback name specified") {
+    get("/p/jsonp", "callback" -> "function") {
+      response.mediaType should equal (Some("application/json"))
+      response.body should equal ("""function({"k1":"v1","k2":"v2"});""")
     }
   }
 }
@@ -21,5 +35,15 @@ class JsonSupportTestServlet extends ScalatraServlet with JsonSupport {
     import net.liftweb.json.JsonDSL._
     ("k1" -> "v1") ~
       ("k2" -> "v2")
+  }
+}
+
+class JsonPTestServlet extends ScalatraServlet with JsonSupport {
+  override def jsonpCallbackParameterNames = Some("callback")
+
+  get("/jsonp") {
+    import net.liftweb.json.JsonDSL._
+    ("k1" -> "v1") ~
+    ("k2" -> "v2")
   }
 }
