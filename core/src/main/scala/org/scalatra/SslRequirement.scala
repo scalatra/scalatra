f5b31b26af11c77f11b0acd0051b703c4521c6fe
Ross A. Baker <baker@alumni.indiana.edu>
2012-02-22 15:45:05 -0500

    Map unsecured ports to secure in SslRequirement, add 8080 -> 8443. #159
    Thanks, @sroebuck.

--- core/src/main/scala/org/scalatra/SslRequirement.scala
+++ core/src/main/scala/org/scalatra/SslRequirement.scala
@@ -10,10 +10,20 @@ trait SslRequirement extends Handler {
   abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
     if(!req.isSecure) {
       val oldUri = new URI(req.getRequestURL.toString)
-      val url = new URI("https", oldUri.getAuthority, oldUri.getPath, oldUri.getQuery, oldUri.getFragment).toString
+      val port = securePortMap.lift(oldUri.getPort) getOrElse 443
+      val url = new URI(
+	"https", oldUri.getRawUserInfo, oldUri.getHost, port,
+	oldUri.getPath, oldUri.getQuery, oldUri.getFragment).toString
       res.sendRedirect(url)
     } else {
       super.handle(req, res)
     }
   }
+  
+  /**
+   * Maps unsecured ports to secure ports.  By default, 80 redirects to
+   * 443, and 8080 to 8443.
+   */
+  protected def securePortMap: PartialFunction[Int, Int] = 
+    Map(80 -> 443, 8080 -> 8443)
 }
