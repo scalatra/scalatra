70c8f6136cda93f3ffd25188637f3b25ab1c9213
Ross A. Baker <baker@alumni.indiana.edu>
2012-02-23 00:30:44 -0500

    Decouple CsrfTokenSupport from the servlet API.

--- core/src/main/scala/org/scalatra/CsrfTokenSupport.scala
+++ core/src/main/scala/org/scalatra/CsrfTokenSupport.scala
@@ -34,7 +34,7 @@ object CsrfTokenSupport {
  * `handleForgery()` hook is invoked.  Otherwise, a token for the next
  * request is prepared with `prepareCsrfToken`.
  */
-trait CsrfTokenSupport { self: ScalatraKernel =>
+trait CsrfTokenSupport { self: ScalatraService =>
   /**
    * The key used to store the token on the session, as well as the parameter
    * of the request.
@@ -62,7 +62,7 @@ trait CsrfTokenSupport { self: ScalatraKernel =>
    * the session key of the same name.
    */
   protected def isForged: Boolean = {
-    request.isWrite && session.get(csrfKey) != params.get(csrfKey)
+    !httpRequest.method.isSafe && session.get(csrfKey) != params.get(csrfKey)
   }
 
   /**
