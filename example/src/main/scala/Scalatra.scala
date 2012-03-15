44d135b83e3eaf7d5fe4468bc2f3e23e3786111e
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-03-15 20:07:24 +0100

    revert previous change

--- example/src/main/scala/Scalatra.scala
+++ example/src/main/scala/Scalatra.scala
@@ -7,10 +7,15 @@ class Scalatra extends LifeCycle {
     context.mount(new BasicAuthExample, "/auth/*")
     context.mount(new DocumentExample, "/docs/*")
     context.mount(new Servlet30ChatExample, "/chat_30/*")
+    
+    /*
+    // TODO: make work without web.xml, per servlet init parameters
     val meteor = new ServletHolder(classOf[org.atmosphere.cpr.MeteorServlet])
     meteor.setInitParameter("org.atmosphere.servlet", "org.scalatra.MeteorChatExample")
     meteor.setInitOrder(0)
-    context.mount(meteor, "/meteor/*")
+    context.mount(meteor, "/meteor/*") 
+    */
+    
     context.mount(new CookiesExample, "/cookies-example/*")
     context.mount(new FilterExample, "/*")
   }
