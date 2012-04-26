53e52604d8409616f5cfc11ce3c40ea795d173ef
Mikko Nylén <mikko.nylen@gmail.com>
2012-04-26 12:59:39 +0300

    Example of FileUploadSupport usage

--- example/src/main/scala/Scalatra.scala
+++ example/src/main/scala/Scalatra.scala
@@ -1,13 +1,15 @@
 import org.scalatra._
-import org.eclipse.jetty.servlet.ServletHolder
 
 class Scalatra extends LifeCycle {
   override def init(context: ApplicationContext) {
-    context.mount(new TemplateExample, "/")
-    context.mount(new BasicAuthExample, "/auth")
-    context.mount(new DocumentExample, "/docs")
-    context.mount(new Servlet30ChatExample, "/chat_30")
-    
+    context.mount(classOf[TemplateExample], "/")
+    context.mount(classOf[BasicAuthExample], "/auth")
+    context.mount(classOf[DocumentExample], "/docs")
+    context.mount(classOf[Servlet30ChatExample], "/chat_30")
+    context.mount(classOf[CookiesExample], "/cookies-example")
+    context.mount(classOf[FilterExample], "/")
+    context.mount(classOf[FileUploadExample], "/upload")
+
     /*
     // TODO: make work without web.xml, per servlet init parameters
     val meteor = new ServletHolder(classOf[org.atmosphere.cpr.MeteorServlet])
@@ -15,8 +17,6 @@ class Scalatra extends LifeCycle {
     meteor.setInitOrder(0)
     context.mount(meteor, "/meteor/*") */
     */
-    
-    context.mount(new CookiesExample, "/cookies-example")
-    context.mount(new FilterExample, "/")
+
   }
 }
