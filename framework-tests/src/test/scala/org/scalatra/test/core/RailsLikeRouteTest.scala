2b07466b24e8ab2e295160649f65e92c10a38580
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-06-29 00:33:03 +0200

    Adds railslikeroutetest

--- framework-tests/src/test/scala/org/scalatra/test/core/RailsLikeRouteTest.scala
+++ framework-tests/src/test/scala/org/scalatra/test/core/RailsLikeRouteTest.scala
@@ -1,34 +1,37 @@
-//package org.scalatra
-//
-//import test.scalatest.ScalatraFunSuite
-//
-//class RailsLikeRouteTestServlet extends ScalatraServlet {
-//  implicit override def string2RouteMatcher(path: String) =
-//    RailsPathPatternParser(path)
-//
-//  // This syntax wouldn't work in Sinatra
-//  get("/:file(.:ext)") {
-//    List("file", "ext") foreach { param =>
-//      response.addHeader(param, params.getOrElse(param, ""))
-//    }
-//  }
-//}
-//
-//class RailsLikeRouteTest extends ScalatraFunSuite {
-//  addServlet(classOf[RailsLikeRouteTestServlet], "/*")
-//
-//  test("matches without extension") {
-//    get("/foo") {
-//      header("file") should equal ("foo")
-//      header("ext") should equal ("")
-//    }
-//  }
-//
-//  test("matches with extension") {
-//    get("/foo.xml") {
-//      header("file") should equal ("foo")
-//      header("ext") should equal ("xml")
-//    }
-//  }
-//}
-//
+package org.scalatra
+
+import test.NettyBackend
+import test.scalatest.ScalatraFunSuite
+
+class RailsLikeRouteTestApp extends ScalatraApp {
+  implicit override def string2RouteMatcher(path: String) =
+    RailsPathPatternParser(path)
+
+  // This syntax wouldn't work in Sinatra
+  get("/:file(.:ext)") {
+    List("file", "ext") foreach { param =>
+      response.headers += param -> params.getOrElse(param, "")
+    }
+  }
+}
+
+abstract class RailsLikeRouteTest extends ScalatraFunSuite {
+  mount(new RailsLikeRouteTestApp)
+
+  test("matches without extension") {
+    get("/foo") {
+      headers("file") should equal ("foo")
+      headers("ext") should equal ("")
+    }
+  }
+
+  test("matches with extension") {
+    get("/foo.xml") {
+      headers("file") should equal ("foo")
+      headers("ext") should equal ("xml")
+    }
+  }
+}
+
+class NettyRailsLikeRouteTest extends RailsLikeRouteTest with NettyBackend
+
