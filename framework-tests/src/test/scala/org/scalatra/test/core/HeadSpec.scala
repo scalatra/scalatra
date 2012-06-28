460806c5af261e9b0e6e8601033aad0b970931de
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-06-28 20:54:13 +0200

    Re-enables CsrfTokenSpec and HeadSpec

--- framework-tests/src/test/scala/org/scalatra/test/core/HeadSpec.scala
+++ framework-tests/src/test/scala/org/scalatra/test/core/HeadSpec.scala
@@ -1,25 +1,28 @@
-//package org.scalatra
-//
-//import test.specs2.ScalatraSpec
-//
-//class HeadSpec extends ScalatraSpec { def is =
-//  "A HEAD request should"                              ^
-//    "return no body"                                   ! noBody^
-//    "preserve headers"                                 ! preserveHeaders^
-//                                                       end
-//
-//  val servletHolder = addServlet(classOf[HeadSpecServlet], "/*")
-//
-//  def noBody = head("/") { response.body must_== "" }
-//
-//  def preserveHeaders = head("/") {
-//    header("X-Powered-By") must_== "caffeine"
-//  }
-//}
-//
-//class HeadSpecServlet extends ScalatraServlet {
-//  get("/") {
-//    response.addHeader("X-Powered-By", "caffeine")
-//    "poof -- watch me disappear"
-//  }
-//}
+package org.scalatra
+
+import test.NettyBackend
+import test.specs2.ScalatraSpec
+
+abstract class HeadSpec extends ScalatraSpec { def is =
+  "A HEAD request should"                              ^
+    "return no body"                                   ! noBody^
+    "preserve headers"                                 ! preserveHeaders^
+                                                       end
+
+  mount(new HeadSpecApp)
+
+  def noBody = head("/") { response.body must_== "" }
+
+  def preserveHeaders = head("/") {
+    headers("X-Powered-By") must_== "caffeine"
+  }
+}
+
+class HeadSpecApp extends ScalatraApp {
+  get("/") {
+    response.addHeader("X-Powered-By", "caffeine")
+    "poof -- watch me disappear"
+  }
+}
+
+class NettyHeadSpec extends HeadSpec with NettyBackend
