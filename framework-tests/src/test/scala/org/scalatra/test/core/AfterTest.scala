9a4f6829aff700a35944dacf414c38ad56a0642c
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-06-28 19:52:42 +0200

    Re-enables AfterTest

--- framework-tests/src/test/scala/org/scalatra/test/core/AfterTest.scala
+++ framework-tests/src/test/scala/org/scalatra/test/core/AfterTest.scala
@@ -1,46 +1,48 @@
-//package org.scalatra
-//
-//import javax.servlet.http.HttpServletResponse
-//import test.scalatest.ScalatraFunSuite
-//
-//class AfterTestServlet extends ScalatraServlet {
-//
-//  after() {
-//    response.setStatus(204)
-//  }
-//
-//  after("/some/path") {
-//    response.setStatus(202)
-//  }
-//
-//  after("/other/path") {
-//    response.setStatus(206)
-//  }
-//
-//  get("/some/path") { }
-//
-//  get("/other/path") { }
-//
-//  get("/third/path") { }
-//
-//}
-//
-//class AfterTest extends ScalatraFunSuite {
-//  addServlet(classOf[AfterTestServlet], "/*")
-//
-//  test("afterAll is applied to all paths") {
-//    get("/third/path") {
-//      status should equal(204)
-//    }
-//  }
-//
-//  test("after only applies to a given path") {
-//    get("/some/path") {
-//      status should equal(202)
-//    }
-//    get("/other/path") {
-//      status should equal(206)
-//    }
-//  }
-//
-//}
+package org.scalatra
+
+import test.NettyBackend
+import test.scalatest.ScalatraFunSuite
+
+class AfterTestApp extends ScalatraApp {
+
+  after() {
+    status = 204
+  }
+
+  after("/some/path") {
+    status = 202
+  }
+
+  after("/other/path") {
+    status = 206
+  }
+
+  get("/some/path") { }
+
+  get("/other/path") { }
+
+  get("/third/path") { }
+
+}
+
+abstract class AfterTest extends ScalatraFunSuite {
+  mount(new AfterTestApp)
+
+  test("afterAll is applied to all paths") {
+    get("/third/path") {
+      status.code should equal(204)
+    }
+  }
+
+  test("after only applies to a given path") {
+    get("/some/path") {
+      status.code should equal(202)
+    }
+    get("/other/path") {
+      status.code should equal(206)
+    }
+  }
+
+}
+
+class NettyAfterTest extends AfterTest with NettyBackend
