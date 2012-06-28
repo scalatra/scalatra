e173cb08d8cd759ad27126a82b72299beadfc37c
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-06-28 19:54:42 +0200

    Re-enables BeforeTest

--- framework-tests/src/test/scala/org/scalatra/test/core/BeforeTest.scala
+++ framework-tests/src/test/scala/org/scalatra/test/core/BeforeTest.scala
@@ -1,46 +1,48 @@
-//package org.scalatra
-//
-//import javax.servlet.http.HttpServletResponse
-//import test.scalatest.ScalatraFunSuite
-//
-//class BeforeTestServlet extends ScalatraServlet {
-//
-//  before() {
-//    response.setStatus(204)
-//  }
-//
-//  before("/some/path") {
-//    response.setStatus(202)
-//  }
-//
-//  before("/other/path") {
-//    response.setStatus(206)
-//  }
-//
-//  get("/some/path") { }
-//
-//  get("/other/path") { }
-//
-//  get("/third/path") { }
-//
-//}
-//
-//class BeforeTest extends ScalatraFunSuite {
-//  addServlet(classOf[BeforeTestServlet], "/*")
-//
-//  test("beforeAll is applied to all paths") {
-//    get("/third/path") {
-//      status should equal(204)
-//    }
-//  }
-//
-//  test("before only applies to a given path") {
-//    get("/some/path") {
-//      status should equal(202)
-//    }
-//    get("/other/path") {
-//      status should equal(206)
-//    }
-//  }
-//
-//}
+package org.scalatra
+
+import test.NettyBackend
+import test.scalatest.ScalatraFunSuite
+
+class BeforeTestApp extends ScalatraApp {
+
+  before() {
+    status = 204
+  }
+
+  before("/some/path") {
+    status = 202
+  }
+
+  before("/other/path") {
+    status = 206
+  }
+
+  get("/some/path") { }
+
+  get("/other/path") { }
+
+  get("/third/path") { }
+
+}
+
+abstract class BeforeTest extends ScalatraFunSuite {
+  mount(new BeforeTestApp)
+
+  test("beforeAll is applied to all paths") {
+    get("/third/path") {
+      status.code should equal(204)
+    }
+  }
+
+  test("before only applies to a given path") {
+    get("/some/path") {
+      status.code should equal(202)
+    }
+    get("/other/path") {
+      status.code should equal(206)
+    }
+  }
+
+}
+
+class NettyBeforeTest extends BeforeTest with NettyBackend
