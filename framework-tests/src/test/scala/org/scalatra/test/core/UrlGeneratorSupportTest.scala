ada3fe46c85de4e1f540d14ab0cc78e81d10926e
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-06-29 09:27:26 +0200

    Re-enables UrlgGeneratorSupportTest

--- framework-tests/src/test/scala/org/scalatra/test/core/UrlGeneratorSupportTest.scala
+++ framework-tests/src/test/scala/org/scalatra/test/core/UrlGeneratorSupportTest.scala
@@ -1,67 +1,69 @@
-//package org.scalatra
-//
-//import test.scalatest.ScalatraFunSuite
-//
-//class UrlGeneratorContextTestServlet extends ScalatraServlet {
-//  val servletRoute: Route = get("/foo") { UrlGenerator.url(servletRoute) }
-//}
-//
-//class UrlGeneratorContextTestFilter extends ScalatraFilter {
-//  val filterRoute: Route = get("/filtered/foo") {
-//    UrlGenerator.url(filterRoute)
-//  }
-//}
-//
-//class UrlGeneratorSupportTest extends ScalatraFunSuite {
-//  addServlet(new UrlGeneratorContextTestServlet, "/*")
+package org.scalatra
+
+import test.NettyBackend
+import test.scalatest.ScalatraFunSuite
+
+class UrlGeneratorContextTestServlet extends ScalatraApp {
+  val servletRoute: Route = get("/foo") { UrlGenerator.url(servletRoute) }
+}
+/*
+class UrlGeneratorContextTestFilter extends ScalatraFilter {
+  val filterRoute: Route = get("/filtered/foo") {
+    UrlGenerator.url(filterRoute)
+  }
+}*/
+
+abstract class UrlGeneratorSupportTest extends ScalatraFunSuite {
+  mount(new UrlGeneratorContextTestServlet)
+  mount("/servlet-path", new UrlGeneratorContextTestServlet)
 //  addServlet(new UrlGeneratorContextTestServlet, "/servlet-path/*")
 //  addServlet(new UrlGeneratorContextTestServlet, "/filtered/*")
 //  addFilter(new UrlGeneratorContextTestFilter, "/*")
+
+  test("Url of a servlet mounted on /*") {
+    get("/foo") {
+      body should equal ("/foo")
+    }
+  }
+
+  test("Url of a servlet mounted on /servlet-path/*") {
+    get("/servlet-path/foo") {
+      body should equal ("/servlet-path/foo")
+    }
+  }
 //
-//  test("Url of a servlet mounted on /*") {
-//    get("/foo") {
-//      body should equal ("/foo")
-//    }
-//  }
-//
-//  test("Url of a servlet mounted on /servlet-path/*") {
-//    get("/servlet-path/foo") {
-//      body should equal ("/servlet-path/foo")
-//    }
-//  }
-//
+//  TODO: Do we still need this??
 //  test("Url of a filter does not duplicate the servlet path") {
 //    get("/filtered/foo") {
 //      body should equal ("/filtered/foo")
 //    }
 //  }
-//}
+}
+
+abstract class UrlGeneratorNonRootContextSupportTest extends ScalatraFunSuite {
+  mount("/context", new UrlGeneratorContextTestServlet)
+  mount("/context/servlet-path", new UrlGeneratorContextTestServlet)
+
+
+  test("Url of a servlet mounted on /*") {
+    get("/context/foo") {
+      body should equal ("/context/foo")
+    }
+  }
+
+  test("Url of a servlet mounted on /servlet-path/*") {
+    get("/context/servlet-path/foo") {
+      body should equal ("/context/servlet-path/foo")
+    }
+  }
 //
-//class UrlGeneratorNonRootContextSupportTest extends ScalatraFunSuite {
-//  tester.setContextPath("/context")
-//
-//  addServlet(new UrlGeneratorContextTestServlet, "/*")
-//  addServlet(new UrlGeneratorContextTestServlet, "/servlet-path/*")
-//  addServlet(new UrlGeneratorContextTestServlet, "/filtered/*")
-//  addFilter(new UrlGeneratorContextTestFilter, "/*")
-//
-//  test("Url of a servlet mounted on /*") {
-//    get("/context/foo") {
-//      body should equal ("/context/foo")
-//    }
-//  }
-//
-//  test("Url of a servlet mounted on /servlet-path/*") {
-//    get("/context/servlet-path/foo") {
-//      body should equal ("/context/servlet-path/foo")
-//    }
-//  }
-//
+//  TODO: Do we still need this??
 //  test("Url of a filter does not duplicate the servlet path") {
 //    get("/context/filtered/foo") {
 //      body should equal ("/context/filtered/foo")
 //    }
 //  }
-//}
-//
-//
+}
+
+class NettyUrlGeneratorSupportTest extends UrlGeneratorSupportTest with NettyBackend
+class NettyUrlGeneratorNonRootContextSupportTest extends UrlGeneratorNonRootContextSupportTest with NettyBackend
--- framework-tests/src/test/scala/org/scalatra/test/core/UrlGeneratorTestServlet.scala
+++ framework-tests/src/test/scala/org/scalatra/test/core/UrlGeneratorTestServlet.scala
@@ -1,44 +1,44 @@
-//package org.scalatra
-//
-//class UrlGeneratorTestServlet extends ScalatraServlet
-//{
-//  val cat: String = "meea"
-//
-//  val simpleString = get("/foo") { }
-//
-//  val singleNamed = get("/foo/:bar") { }
-//
-//  val multipleNameds = get("/foo/:bar/and/:rebar") { }
-//
-//  val booleanTest = get(params.getOrElse("booleanTest", "false") == "true") { }
-//
-//  val optional = get("/optional/?:foo?/?:bar?") { }
-//
-//  val optionalExt = get("/optional-ext.?:ext?") { }
-//
-//  val singleSplat = get("/single-splat/*") { }
-//
-//  val multipleSplats = get("/mixing-multiple-splats/*/foo/*/*") { }
-//
-//  val mixNamedAndSplat = get("/mix-named-and-splat-params/:foo/*") { }
-//
-//  val dotInNamedParam = get("/dot-in-named-param/:f.oo/:bar") { }
-//
-//  val dotOutsideNamedParam = get("/dot-outside-named-param/:file.:ext") { }
-//
-//  val literalDotInPath = get("/literal.dot.in.path") { }
-//
-//  val stringAndBoolean = get("/conditional", params.getOrElse("condition", "false") == "true") { }
-//
-//  val regex1 = get("""^\/fo(.*)/ba(.*)""".r) { }
-//
-//  val regex2 = get("""^/foo.../bar$""".r) { }
-//
-//  val anyPost = post() { }
-//
-//  val pathPattern = get(new PathPattern(".".r, Nil)) { }
-//
-//  val customMatcher = get(new RouteMatcher { def apply() = None }) { }
-//
-//  val stringAndCustomMatcher = get("/fail", new RouteMatcher { def apply() = None }) { }
-//}
+package org.scalatra
+
+class UrlGeneratorTestApp extends ScalatraApp
+{
+  val cat: String = "meea"
+
+  val simpleString = get("/foo") { }
+
+  val singleNamed = get("/foo/:bar") { }
+
+  val multipleNameds = get("/foo/:bar/and/:rebar") { }
+
+  val booleanTest = get(params.getOrElse("booleanTest", "false") == "true") { }
+
+  val optional = get("/optional/?:foo?/?:bar?") { }
+
+  val optionalExt = get("/optional-ext.?:ext?") { }
+
+  val singleSplat = get("/single-splat/*") { }
+
+  val multipleSplats = get("/mixing-multiple-splats/*/foo/*/*") { }
+
+  val mixNamedAndSplat = get("/mix-named-and-splat-params/:foo/*") { }
+
+  val dotInNamedParam = get("/dot-in-named-param/:f.oo/:bar") { }
+
+  val dotOutsideNamedParam = get("/dot-outside-named-param/:file.:ext") { }
+
+  val literalDotInPath = get("/literal.dot.in.path") { }
+
+  val stringAndBoolean = get("/conditional", params.getOrElse("condition", "false") == "true") { }
+
+  val regex1 = get("""^\/fo(.*)/ba(.*)""".r) { }
+
+  val regex2 = get("""^/foo.../bar$""".r) { }
+
+  val anyPost = post() { }
+
+  val pathPattern = get(new PathPattern(".".r, Nil)) { }
+
+  val customMatcher = get(new RouteMatcher { def apply() = None }) { }
+
+  val stringAndCustomMatcher = get("/fail", new RouteMatcher { def apply() = None }) { }
+}
