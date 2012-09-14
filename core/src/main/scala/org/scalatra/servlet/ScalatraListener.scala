c63579fc00987599c30e0968f2b0bf1d0e8b3e09
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-09-14 09:55:46 -0700

    Use ScalatraBootstrap as LifeCycle class name

--- core/src/main/scala/org/scalatra/servlet/ScalatraListener.scala
+++ core/src/main/scala/org/scalatra/servlet/ScalatraListener.scala
@@ -19,11 +19,15 @@ class ScalatraListener extends ServletContextListener {
     servletContext = sce.getServletContext
     val cycleClassName = 
       Option(servletContext.getAttribute(LifeCycleKey)).flatMap(_.asInstanceOf[String].blankOption) getOrElse DefaultLifeCycle
-    val cycleClass = Class.forName(cycleClassName)
-    if (!classOf[LifeCycle].isAssignableFrom(cycleClass)) {
+    val lifeCycleClass: Class[_] = Class.forName(cycleClassName)
+    def oldLifeCycleClass: Class[_] = Class.forName(OldDefaultLifeCycle)
+    val cycleClass: Class[_] = if (lifeCycleClass != null) lifeCycleClass else oldLifeCycleClass
+    if (cycleClass != null && !classOf[LifeCycle].isAssignableFrom(cycleClass)) {
       logger.error("This is no lifecycle class.")
       throw new ClassCastException("This is no lifecycle class.")
     }
+    if (cycleClass.getName == OldDefaultLifeCycle)
+      logger.warn("The Scalatra name for a boot class will be removed eventually. Please use ScalatraBootstrap instead as class name.")
     cycle = cycleClass.newInstance.asInstanceOf[LifeCycle]
     logger.info("Initializing life cycle class: %s".format(cycleClassName))
     cycle.init(servletContext)
@@ -38,6 +42,7 @@ class ScalatraListener extends ServletContextListener {
 }
 
 object ScalatraListener {
-  val DefaultLifeCycle = "Scalatra"
+  val DefaultLifeCycle = "ScalatraBootstrap"
+  val OldDefaultLifeCycle = "Scalatra"
   val LifeCycleKey = "org.scalatra.LifeCycle"
 }
