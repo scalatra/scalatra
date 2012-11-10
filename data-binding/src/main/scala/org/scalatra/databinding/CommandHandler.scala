3a3006bcadddb8b8600e9825d85982c1a922be5a
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-11-10 11:57:56 -0800

    update notes

--- data-binding/src/main/scala/org/scalatra/databinding/CommandHandler.scala
+++ data-binding/src/main/scala/org/scalatra/databinding/CommandHandler.scala
@@ -8,27 +8,27 @@ import scalaz._
 import Scalaz._
 
 trait CommandHandler { 
-  @transient private[this] lazy val logger: Logger = Logger[this.type]
+  @transient private[this] val commandLogger: Logger = Logger[this.type]
   def execute[S: Manifest](cmd: ModelCommand[S]): ModelValidation[S] = {
-    logger.debug("Executing [%s].\n%s" format (cmd.getClass.getName, cmd))
+    commandLogger.debug("Executing [%s].\n%s" format (cmd.getClass.getName, cmd))
     if (cmd.isValid) {
       val res = (allCatch withApply (serverError(cmd.getClass.getName, _))) {
         handle.lift(cmd).map(_.map(_.asInstanceOf[S])) | ValidationError("Don't know how to handle: " + cmd.getClass.getName, UnknownError).failNel
       }
       val ftext = "with %d failures\n%s".format(~res.fail.toOption.map(_.list.size), ~res.fail.toOption.map(_.list))
-      logger.debug("Command [%s] executed %s." format (cmd.getClass.getName, res.isSuccess ? "successfully." | ftext))
+      commandLogger.debug("Command [%s] executed %s." format (cmd.getClass.getName, res.isSuccess ? "successfully." | ftext))
       res
     } else {
       val f = cmd.errors.map(_.validation) collect {
         case Failure(e) ⇒ e
       }
-      logger.debug("Command [%s] executed with %d failures.\n%s" format (cmd.getClass.getName, f.size, f.toList))
+      commandLogger.debug("Command [%s] executed with %d failures.\n%s" format (cmd.getClass.getName, f.size, f.toList))
       nel(f.head, f.tail: _*).fail
     }
   }
 
   private[this] def serverError[R](cmdName: String, ex: Throwable): ModelValidation[R] = {
-    logger.error("There was an error while executing " + cmdName, ex)
+    commandLogger.error("There was an error while executing " + cmdName, ex)
     ValidationError("An error occurred while handling: " + cmdName, UnknownError).failNel[R]
   }
 
