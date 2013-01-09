c4f4e4b568ae280bb79ce0937e3b6c93f8049205
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-01-09 12:00:44 +0100

    add a bit of scaladoc for validation error

--- commands/src/main/scala/org/scalatra/validation/ValidationError.scala
+++ commands/src/main/scala/org/scalatra/validation/ValidationError.scala
@@ -4,10 +4,26 @@ package validation
 import org.json4s._
 import JsonDSL._
 
-
+/**
+ * Encapsulates errors in an API
+ * @param message The message for this error
+ * @param field An optional field name, useful when it applies to a particular field
+ * @param code Decouple business logic error types from http errors
+ * @param args Optional args, these need to be serializable to json if you're using the [[org.scalatra.validation.ValidationErrorSerializer]]
+ */
 case class ValidationError(message: String, field: Option[FieldName], code: Option[ErrorCode], args: Seq[Any])
+
+/**
+ * Encapsulates a field name for use in a validation error
+ * 
+ * @param name The name of the field
+ */
 case class FieldName(name: String)
 
+/**
+ * A base trait for error codes, all error codes need to extend this.
+ * It's available for you so you can add more in your app
+ */
 trait ErrorCode
 case object NotFound extends ErrorCode
 case object UnknownError extends ErrorCode
@@ -17,7 +33,19 @@ case object ValidationFail extends ErrorCode
 case object ServiceUnavailable extends ErrorCode
 case object GatewayTimeout extends ErrorCode
 
+/**
+ * Allows for unordered building of [[org.scalatra.validation.ValidationError]]
+ */
 object ValidationError {
+  /**
+   * Allows for unordered building of [[org.scalatra.validation.ValidationError]]
+   * You can add do `ValidationError("the message", ValidationFail, FieldName("email"))`
+   * or `ValidationError("the message", FieldName("email"), ValidationFail)`
+   * and also `ValidationError("the message", FieldName("email"), Some(ValidationFail))`
+   *
+   * This method will create a correct ValidationError that looks like:
+   * `ValidationError("the message", FieldName("email"), Some(ValidationFail), Seq())`
+   */
   def apply(msg: String, arguments: Any*): ValidationError = {
     val field = arguments collectFirst {
       case f: FieldName => f
@@ -35,6 +63,11 @@ object ValidationError {
   }
 }
 
+/**
+ * Assumes your error codes will always be case objects.
+ *
+ * @param knownCodes A list of known error codes for your system
+ */
 class ErrorCodeSerializer(knownCodes: ErrorCode*) extends Serializer[ErrorCode] {
   val ecs = Map(knownCodes map { c ⇒ c.getClass.getSimpleName.replaceAll("\\$$","").toUpperCase -> c }: _*)
   val Class = classOf[ErrorCode]
@@ -51,6 +84,23 @@ class ErrorCodeSerializer(knownCodes: ErrorCode*) extends Serializer[ErrorCode] 
   }
 }
 
+/**
+ * Serializes a validation error into a structure:
+ * <pre>
+ *   {
+ *     "message": "the error message",
+ *     "field": "field_name",
+ *     "code": "ValidationFail",
+ *     "args": []
+ *   }
+ * </pre>
+ *
+ * You can configure whether or not to include the args. And on the way in it assumes args are JValues.
+ * The fields: field, code and args are only added if they have actual data.
+ *
+ * @param includeCode Include the code field if an error code is provided
+ * @param includeArgs Include the args field when args are provided
+ */
 class ValidationErrorSerializer(includeCode: Boolean = true, includeArgs: Boolean = true) extends CustomSerializer[ValidationError](
   (formats: Formats) ⇒ ({
     case jo @ JObject(JField("message", _) :: _) ⇒
@@ -65,7 +115,7 @@ class ValidationErrorSerializer(includeCode: Boolean = true, includeArgs: Boolea
       implicit val fmts = formats
       val jv: JValue = ("message" -> message)
       val wf: JValue = fieldName map (fn ⇒ ("field" -> fn.name): JValue) getOrElse JNothing
-      val ec: JValue = if (includeCode) ("code" -> (code map (Extraction.decompose(_)(formats)))) else JNothing
-      val arg: JValue = if (includeArgs) ("args" -> Extraction.decompose(args)(formats)) else JNothing
+      val ec: JValue = if (includeCode && code.isDefined) ("code" -> (code map (Extraction.decompose(_)(formats)))) else JNothing
+      val arg: JValue = if (includeArgs && args.nonEmpty) ("args" -> Extraction.decompose(args)(formats)) else JNothing
       jv merge wf merge ec merge arg
   }))
