773d17cd62637d4e8f0d938eb1260f4350eee356
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-11 15:57:24 -0500

    [breaking] Akka needs an ExecutionContext, not an ActorSystem.

--- akka/src/test/scala/org/scalatra/akka/AkkaSupportAfterFilterForFiltersSpec.scala
+++ akka/src/test/scala/org/scalatra/akka/AkkaSupportAfterFilterForFiltersSpec.scala
@@ -11,7 +11,7 @@ class AkkaSupportAfterFilterFilter extends ScalatraFilter with AkkaSupport {
   var actionTime: Long = _
   var afterTime: Long = _
   var afterCount: Long = _
-  private implicit lazy val _executor = akkaDispatcherName map system.dispatchers.lookup getOrElse system.dispatcher
+  protected implicit lazy val executor = system.dispatcher
 
   asyncGet("/async") {
     Thread.sleep(2000)
