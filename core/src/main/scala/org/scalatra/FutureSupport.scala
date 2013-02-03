8a2935af4e27161eb6a4fb39362900f871e4ec7f
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-02-04 00:38:52 +0100

    refactor future support a little and make timeout a duration again and add a default value for the timeout in the async result

--- core/src/main/scala/org/scalatra/FutureSupport.scala
+++ core/src/main/scala/org/scalatra/FutureSupport.scala
@@ -15,7 +15,8 @@ abstract class AsyncResult(implicit override val scalatraContext: ScalatraContex
   implicit val response: HttpServletResponse = scalatraContext.response
   val servletContext: ServletContext = scalatraContext.servletContext
 
-  implicit def timeout: Timeout
+  // This is a Duration instead of a timeout because a duration has the concept of infinity
+  implicit def timeout: Duration = 30 seconds
   def is: Future[_]
 }
 
@@ -28,6 +29,7 @@ trait FutureSupport extends AsyncSupport {
   // Still thinking of the best way to specify this before making it public.
   // In the meantime, this gives us enough control for our test.
   // IPC: it may not be perfect but I need to be able to configure this timeout in an application
+  // This is a Duration instead of a timeout because a duration has the concept of infinity
   protected def asyncTimeout: Duration = 30 seconds
 
 
@@ -35,47 +37,50 @@ trait FutureSupport extends AsyncSupport {
 
   override protected def renderResponse(actionResult: Any) {
     actionResult match {
-      case r: AsyncResult => renderResponse(r.is)
-      case f: Future[_] => {
-        val gotResponseAlready = new AtomicBoolean(false)
-        val context = request.startAsync()
-        context.setTimeout(asyncTimeout.toMillis)
-        context addListener (new AsyncListener {
-          def onComplete(event: AsyncEvent) {}
+      case r: AsyncResult ⇒ handleFuture(r.is, r.timeout)
+      case f: Future[_]   ⇒ handleFuture(f, asyncTimeout)
+      case a              ⇒ super.renderResponse(a)
+    }
+  }
 
-          def onTimeout(event: AsyncEvent) {
-            onAsyncEvent(event) {
-              if (gotResponseAlready.compareAndSet(false, true)) {
-                renderHaltException(HaltException(Some(504), None, Map.empty, "Gateway timeout"))
-                event.getAsyncContext.complete()
-              }
-            }
-          }
+  private[this] def handleFuture(f: Future[_], timeout: Duration) {
+    val gotResponseAlready = new AtomicBoolean(false)
+    val context = request.startAsync()
+    if (timeout.isFinite())
+      context.setTimeout(timeout.toMillis)
+    else
+      context.setTimeout(-1)
+    context addListener (new AsyncListener {
+      def onComplete(event: AsyncEvent) {}
 
-          def onError(event: AsyncEvent) {}
-
-          def onStartAsync(event: AsyncEvent) {}
-        })
-
-        f onComplete {
-          case t ⇒ {
-            withinAsyncContext(context) {
-              if (gotResponseAlready.compareAndSet(false, true)) {
-                t map { result =>
-                  runFilters(routes.afterFilters)
-                  super.renderResponse(result)
-                } recover {
-                  case e: HaltException ⇒ renderHaltException(e)
-                  case e => renderResponse(errorHandler(e))
-                }
-                context.complete()
-              }
-            }
+      def onTimeout(event: AsyncEvent) {
+        onAsyncEvent(event) {
+          if (gotResponseAlready.compareAndSet(false, true)) {
+            renderHaltException(HaltException(Some(504), None, Map.empty, "Gateway timeout"))
+            event.getAsyncContext.complete()
           }
         }
       }
-      case a ⇒ {
-        super.renderResponse(a)
+
+      def onError(event: AsyncEvent) {}
+
+      def onStartAsync(event: AsyncEvent) {}
+    })
+
+    f onComplete {
+      case t ⇒ {
+        withinAsyncContext(context) {
+          if (gotResponseAlready.compareAndSet(false, true)) {
+            t map { result =>
+              runFilters(routes.afterFilters)
+              super.renderResponse(result)
+            } recover {
+              case e: HaltException ⇒ renderHaltException(e)
+              case e => renderResponse(errorHandler(e))
+            }
+            context.complete()
+          }
+        }
       }
     }
   }
