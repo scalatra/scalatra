e4cd251b5b5679a56aa7cad977a13c46a0cb3bac
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-03-22 08:47:27 +0100

    Fix specs2 base trait infrastructure

--- specs2/src/main/scala/org/scalatra/test/specs2/BaseScalatraSpec.scala
+++ specs2/src/main/scala/org/scalatra/test/specs2/BaseScalatraSpec.scala
@@ -2,13 +2,14 @@ package org.scalatra
 package test
 package specs2
 
-import org.specs2.specification.{BaseSpecification, Step, Fragments}
+import org.specs2.specification.{SpecificationStructure, BaseSpecification, Step, Fragments}
+import org.specs2.mutable.FragmentsBuilder
 
 /**
- * A base specification structure that starts the tester before the
- * specification and stops it afterward.  Clients probably want to extend
- * ScalatraSpec or MutableScalatraSpec.
- */
-trait BaseScalatraSpec extends BaseSpecification with ScalatraTests {
+* A base specification structure that starts the tester before the
+* specification and stops it afterward.  Clients probably want to extend
+* ScalatraSpec or MutableScalatraSpec.
+*/
+trait BaseScalatraSpec extends SpecificationStructure with FragmentsBuilder with ScalatraTests {
   override def map(fs: =>Fragments) = Step(start()) ^ super.map(fs) ^ Step(stop())
 }
