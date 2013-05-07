36ff9e7d88fdd0abb3ccbc5f7b9b8f6e5e8aaede
takezoe <takezoe@gmail.com>
2013-05-07 12:30:10 +0900

    Fixed jettyVersion.

--- project/build.scala
+++ project/build.scala
@@ -311,7 +311,7 @@ object ScalatraBuild extends Build {
 
     private val httpcomponentsVersion = "4.2.3"
 
-    private val jettyVersion = "8.1.10.v20130312"
+    private val jettyVersion = "9.0.2.v20130417"
 
     private val json4sVersion = "3.2.4"
 
