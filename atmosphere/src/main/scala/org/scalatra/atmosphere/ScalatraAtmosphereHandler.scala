a27e678ca06e91025756d0fcd93d7a7b0762bac3
Irmo Manie <irmo.manie@gmail.com>
2013-07-23 17:11:25 +0200

    Fix atmosphere disconnect handling
    Only remove session attribute _after_ the client is retrieved from the session and sent the `Disconnected` message.
    Also removing the pointless first `isCancelled` check.
    Conflicts:
    	atmosphere/src/main/scala/org/scalatra/atmosphere/ScalatraAtmosphereHandler.scala

--- atmosphere/src/main/scala/org/scalatra/atmosphere/ScalatraAtmosphereHandler.scala
+++ atmosphere/src/main/scala/org/scalatra/atmosphere/ScalatraAtmosphereHandler.scala
@@ -34,12 +34,12 @@ object ScalatraAtmosphereHandler {
     }
 
     def onDisconnect(event: AtmosphereResourceEvent) {
-        val disconnector = if (event.isCancelled) ClientDisconnected else ServerDisconnected
-        client(event.getResource) foreach (_.receive.lift(Disconnected(disconnector, Option(event.throwable))))
-        if (!event.getResource.isResumed) {
-           event.getResource.session.invalidate()
-       } else {
-         event.getResource.session.removeAttribute(org.scalatra.atmosphere.AtmosphereClientKey)
+      val disconnector = if (event.isCancelled) ClientDisconnected else ServerDisconnected
+      client(event.getResource) foreach (_.receive.lift(Disconnected(disconnector, Option(event.throwable))))
+      if (!event.getResource.isResumed) {
+        event.getResource.session.invalidate()
+      } else {
+        event.getResource.session.removeAttribute(org.scalatra.atmosphere.AtmosphereClientKey)
       }
     }
 
