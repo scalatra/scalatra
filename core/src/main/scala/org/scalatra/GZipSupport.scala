0492be15599879def11cd84f46318f2db0b29012
Nicolas Long <nicolas.long@guardian.co.uk>
2013-09-24 17:58:06 +0100

    Remove unused import

--- core/src/main/scala/org/scalatra/GZipSupport.scala
+++ core/src/main/scala/org/scalatra/GZipSupport.scala
@@ -6,7 +6,6 @@ import javax.servlet.ServletOutputStream
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpServletResponseWrapper
-import util.RicherString._
 
 /**
  * Scalatra handler for gzipped responses.
