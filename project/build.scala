5676aead0f0a3c6395815148dde79e6c485d6584
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2013-12-24 18:43:09 +0000

    Bumping Atmosphere to late version, 2.0.5

--- project/build.scala
+++ project/build.scala
@@ -342,7 +342,7 @@ object ScalatraBuild extends Build {
       case _                            => "1.0.1"
     }
 
-    private val atmosphereVersion = "2.0.3"
+    private val atmosphereVersion = "2.0.5"
 
     private val atmosphereCompatVersion = "2.0.0"
 
