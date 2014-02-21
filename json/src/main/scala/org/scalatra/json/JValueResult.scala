50d4d73cb48287bf20d896124ef8267920749dde
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2014-02-21 20:14:48 +0100

    Converts values of type Any to JValue if there is a FieldSerializer available.

--- json/src/main/scala/org/scalatra/json/JValueResult.scala
+++ json/src/main/scala/org/scalatra/json/JValueResult.scala
@@ -15,18 +15,41 @@ trait JValueResult extends ScalatraBase { self: JsonSupport[_] =>
 
   override protected def renderPipeline: RenderPipeline = renderToJson orElse super.renderPipeline
 
+  // should the result value be converted to a JValue?
   private[this] def isJValueResponse = responseFormat == "json" || responseFormat == "xml"
 
   private[this] def customSerializer = jsonFormats.customSerializer
 
+  // TODO re-use Formats function
+  // TODO cache results
+  private[this] def fieldSerializer(clazz: Class[_]): Option[FieldSerializer[_]] = {
+    import ClassDelta._
+
+    val ord = Ordering[Int].on[(Class[_], FieldSerializer[_])](x => delta(x._1, clazz))
+    jsonFormats.fieldSerializers filter (_._1.isAssignableFrom(clazz)) match {
+      case Nil => None
+      case xs  => Some((xs min ord)._2)
+    }
+  }
+
+  private[this] object ClassDelta {
+    def delta(class1: Class[_], class2: Class[_]): Int = {
+      if (class1 == class2) 0
+      else if (class1.getInterfaces.contains(class2)) 0
+      else if (class2.getInterfaces.contains(class1)) 0
+      else if (class1.isAssignableFrom(class2)) {
+        1 + delta(class1, class2.getSuperclass)
+      }
+      else if (class2.isAssignableFrom(class1)) {
+        1 + delta(class1.getSuperclass, class2)
+      }
+      else sys.error("Don't call delta unless one class is assignable from the other")
+    }
+  }
+
   private[this] def renderToJson: RenderPipeline = {
     case JNull | JNothing =>
     case a: JValue => super.renderPipeline(a)
-    case a: Any if isJValueResponse && customSerializer.isDefinedAt(a) =>
-      customSerializer.lift(a) match {
-        case Some(jv: JValue) => jv
-        case None => super.renderPipeline(a)
-      }
     case status: Int => super.renderPipeline(status)
     case bytes: Array[Byte] => super.renderPipeline(bytes)
     case is: java.io.InputStream => super.renderPipeline(is)
@@ -42,6 +65,13 @@ trait JValueResult extends ScalatraBase { self: JsonSupport[_] =>
       response.writer.write(x.toString)
     case x: NodeSeq ⇒
       response.writer.write(x.toString)
+    case a: Any if isJValueResponse && customSerializer.isDefinedAt(a) =>
+      customSerializer.lift(a) match {
+        case Some(jv: JValue) => jv
+        case None => super.renderPipeline(a)
+      }
+    case a: Any if isJValueResponse && fieldSerializer(a.getClass).isDefined =>
+      Extraction.decompose(a)
     case p: Product if isJValueResponse => Extraction.decompose(p)
     case p: TraversableOnce[_] if isJValueResponse => Extraction.decompose(p)
   }
--- json/src/test/scala/org/scalatra/json/JValueResultSpec.scala
+++ json/src/test/scala/org/scalatra/json/JValueResultSpec.scala
@@ -16,8 +16,19 @@ class BottleSerializer extends CustomSerializer[Bottle](implicit formats => ({
     val of = (json \ "of").extract[String]
     new Bottle(of)
 }, {
-  case a: Bottle => JObject(JField("of", JString(a.of)))
+  case a: Bottle => ("of", a.of)
 }))
+
+class SensorMessage(val subject: String = "foo-probe") {
+  val version = 1.0
+
+  val value1 = 3.1415
+  val valueN = 2.7182
+
+  def done = {
+    false
+  }
+}
 
 class JValueResultSpec extends MutableScalatraSpec {
 
@@ -28,10 +39,18 @@ class JValueResultSpec extends MutableScalatraSpec {
   }
 
   val jValue = JObject(JField("name", JString("tom")) :: Nil)
+  val bottleRum = ("of", "rum").asJValue
+  val bottleSoda = ("of", "soda").asJValue
+  val message = ("version", 1.0) ~
+    ("subject", "foo-probe") ~
+    ("value1", 3.1415) ~
+    ("valueN", 2.7182)
 
   addServlet(new ScalatraServlet with JacksonJsonSupport {
 
-    implicit protected def jsonFormats: Formats = DefaultFormats + new BottleSerializer
+    implicit protected def jsonFormats: Formats = DefaultFormats +
+      new BottleSerializer +
+      FieldSerializer[SensorMessage]()
 
     notFound {
       status = 404
@@ -41,43 +60,56 @@ class JValueResultSpec extends MutableScalatraSpec {
     get("/jvalue") {
       jValue
     }
+
     get("/actionresult") {
       Created(jValue)
     }
+
     get("/unit") {
       response.writer.println("printed")
     }
+
     get("/namedthing") {
       contentType = formats("json")
       NamedThing()
     }
+
     get("/empty-not-found") {
       NotFound()
     }
+
     get("/halted-not-found") {
       halt(NotFound())
     }
+
     get("/null-value.:format") {
       null
     }
+
     get("/null-value") {
       ""
     }
+
     get("/empty-halt") {
       halt(400, null)
     }
 
-    get("/class") {
+    get("/custom-serializer") {
       contentType = formats("json")
       new Bottle("rum")
     }
 
-    get("/class-list") {
+    get("/field-serializer") {
+      contentType = formats("json")
+      new SensorMessage("foo-probe")
+    }
+
+    get("/traversable") {
       contentType = formats("json")
       List(new Bottle("rum"), new Bottle("soda"))
     }
 
-    get("/mixed-list") {
+    get("/mixed-traversable") {
       contentType = formats("json")
       List(new Bottle("rum"), NamedThing())
     }
@@ -99,36 +131,43 @@ class JValueResultSpec extends MutableScalatraSpec {
         parse(body) must_== jValue
       }
     }
+
     "render an ActionResult result" in {
       get("/actionresult") {
         status must_== 201
         parse(body) must_== jValue
       }
     }
+
     "render a Unit result" in {
       get("/unit") {
         body must_== "printed\n"
       }
     }
+
     "render a NamedThing result" in {
       get("/namedthing") { parse(body) must_== jValue }
     }
+
     "render an empty not found" in {
       get("/empty-not-found") {
         status must_== 404
         body must_== "the custom not found"
       }
     }
+
     "render a null value" in {
       get("/null-value.json") {
          body must_== ""
       }
     }
+
     "render a null value" in {
       get("/null-value.html") {
         body must_== ""
       }
     }
+
     "render a halted empty not found" in {
       get("/halted-not-found") {
         status must_== 404
@@ -136,23 +175,28 @@ class JValueResultSpec extends MutableScalatraSpec {
       }
     }
 
-    val bottleRum = ("of", "rum").asJValue
-    val bottleSoda = ("of", "soda").asJValue
-
-    "render a class" in {
-      get("/class") {
+    "render a class using a custom serializer" in {
+      get("/custom-serializer") {
         parse(body) must_== bottleRum
       }
     }
 
-    "render a class list" in {
-      get("/class-list") {
+    "render a class using a field serializer" in {
+      get("/field-serializer") {
+        println(body)
+        println(parse(body))
+        parse(body) must_== message
+      }
+    }
+
+    "render a traversable" in {
+      get("/traversable") {
         parse(body) must_== List(bottleRum, bottleSoda).asJValue
       }
     }
 
-    "render a mixed list" in {
-      get("/mixed-list") {
+    "render a mixed traversable" in {
+      get("/mixed-traversable") {
         parse(body) must_== List(bottleRum, jValue).asJValue
       }
     }
