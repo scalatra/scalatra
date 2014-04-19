f8521f6076fef842f8bf82e7ed705f7d7ae22fa7
takezoe <takezoe@gmail.com>
2014-04-19 18:37:15 +0900

    Upgrade Specs2 and Scalaz

--- project/build.scala
+++ project/build.scala
@@ -389,7 +389,11 @@ object ScalatraBuild extends Build {
       case sv if sv startsWith "2.8."   => "1.5"
       case "2.9.0-1"                    => "1.8.2"
       case sv if sv startsWith "2.9."   => "1.12.4.1"
+<<<<<<< HEAD
       case _                            => "2.3.12"
+=======
+      case _                            => "2.3.11"
+>>>>>>> Upgrade Specs2 and Scalaz
     }
 
   }
