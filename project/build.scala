f1820894c4162a2a9902402b906f034bc69afec1
Matthew de Detrich <mdedetrich@gmail.com>
2014-09-09 12:19:27 +1000

    - Bumped specs2 version due to scalaz incompatibility

--- project/build.scala
+++ project/build.scala
@@ -386,7 +386,7 @@ object ScalatraBuild extends Build {
       case sv if sv startsWith "2.8."   => "1.5"
       case "2.9.0-1"                    => "1.8.2"
       case sv if sv startsWith "2.9."   => "1.12.4.1"
-      case _                            => "2.3.12"
+      case _                            => "2.4.2"
     }
 
   }
