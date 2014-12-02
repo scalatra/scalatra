aa9a8a17648fb7416a931f64d49768c1523580cf
Henry Zektser <japhar81@gmail.com>
2014-12-01 23:53:04 -0500

    Fixed metrics

--- metrics/src/main/scala/org/scalatra/metrics/MetricsSupport.scala
+++ metrics/src/main/scala/org/scalatra/metrics/MetricsSupport.scala
@@ -5,15 +5,16 @@ import com.codahale.metrics._
 import nl.grons.metrics.scala._
 import org.scalatra.ScalatraBase
 
-trait MetricsSupport {
+trait MetricsSupport extends nl.grons.metrics.scala.InstrumentedBuilder {
   self: ScalatraBase ⇒
 
-  protected def metricsRegistry = Metrics.metricRegistry
+  val metricsRegistry = Metrics.metricRegistry
 
   def metricName(name: String) = MetricName(name)
 
-  def timer[A](name: String)(thunk: => A) = metricsRegistry.timer(name).time { new Callable[A] { def call(): A = thunk } }
-  def counter(name: String) = metricsRegistry.counter(name)
-  def histogram(name: String) = metricsRegistry.histogram(name)
-  def meter(name: String) = metricsRegistry.meter(name)
+  def timer[A](name: String)(thunk: => A) = metrics.timer(name).time { new Callable[A] { def call(): A = thunk } }
+  def counter(name: String) = metrics.counter(name)
+  def histogram(name: String) = metrics.histogram(name)
+  def meter(name: String) = metrics.meter(name)
+  def gauge(name: String) = metrics.gauge(name)
 }
