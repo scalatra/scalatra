86533f984abc4e69cb1bfbc29757f403507c428c
Henry Zektser <japhar81@gmail.com>
2014-12-01 23:54:17 -0500

    whoops

--- metrics/src/main/scala/org/scalatra/metrics/MetricsSupport.scala
+++ metrics/src/main/scala/org/scalatra/metrics/MetricsSupport.scala
@@ -13,8 +13,8 @@ trait MetricsSupport extends nl.grons.metrics.scala.InstrumentedBuilder {
   def metricName(name: String) = MetricName(name)
 
   def timer[A](name: String)(thunk: => A) = metrics.timer(name).time { new Callable[A] { def call(): A = thunk } }
+  def gauge[A](name: String)(thunk: => A) = metrics.gauge(name) { new Callable[A] { def call(): A = thunk } }
   def counter(name: String) = metrics.counter(name)
   def histogram(name: String) = metrics.histogram(name)
   def meter(name: String) = metrics.meter(name)
-  def gauge(name: String) = metrics.gauge(name)
 }
