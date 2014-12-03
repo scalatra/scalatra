3b6653c0fd3f091746c69e7e144d34171f25eb73
Henry Zektser <japhar81@gmail.com>
2014-12-03 11:50:07 -0500

    Registries REQURED to be val and singleton

--- metrics/src/main/scala/org/scalatra/metrics/MetricsBootstrap.scala
+++ metrics/src/main/scala/org/scalatra/metrics/MetricsBootstrap.scala
@@ -4,6 +4,11 @@ import com.codahale.metrics.MetricRegistry
 import com.codahale.metrics.health.HealthCheckRegistry
 
 trait MetricsBootstrap {
-  implicit val healthCheckRegistry: HealthCheckRegistry = new HealthCheckRegistry()
-  implicit val metricRegistry: MetricRegistry = new MetricRegistry()
+  implicit val healthCheckRegistry: HealthCheckRegistry = MetricsRegistries.healthCheckRegistry
+  implicit val metricRegistry: MetricRegistry = MetricsRegistries.metricRegistry
+}
+
+object MetricsRegistries {
+  val healthCheckRegistry = new HealthCheckRegistry()
+  val metricRegistry = new MetricRegistry()
 }
