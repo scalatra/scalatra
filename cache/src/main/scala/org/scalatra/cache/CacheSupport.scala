9dc8844fa5d1cb02793f520a23c4b28cfdc48fa2
Henry Zektser <japhar81@gmail.com>
2014-12-18 21:31:26 -0500

    Tweaks

--- cache/src/main/scala/org/scalatra/cache/CacheSupport.scala
+++ cache/src/main/scala/org/scalatra/cache/CacheSupport.scala
@@ -3,14 +3,10 @@ package org.scalatra.cache
 trait CacheSupport {
   implicit val cacheBackend: Cache
 
-  def cache[A](key: String, ttlMs: Long)(f: => A): A = {
+  def cache[A](key: String, ttlMs: Long = 60000)(f: => A): A = {
     cacheBackend.get[A](key) match {
       case Some(v)  => v
       case None     => cacheBackend.put(key, f, ttlMs)
     }
-  }
-
-  def cache[A](key:String)(f: => A): A = {
-    cache(key, 60000)(f)
   }
 }
