cdccdea631990f880ac026869665c9e3a34f05fb
Lloyd <lloydmeta@gmail.com>
2015-05-29 11:08:35 +0900

    Add a test to see if request values get leaked between requests
    The idea is to increase the chances of a request handler thread
    and its accompanying request object getting recycled and thus
    handling multiple request before the worker app threads get to
     work on the request.
    
    So, Client threads >> Jetty Http handler threads >> worker threads
    
    For some reason, it looks like request values *do not* get mixed up,
    even though it seems like the should.

--- core/src/test/scala/org/scalatra/AkkaSupportSpec.scala
+++ core/src/test/scala/org/scalatra/AkkaSupportSpec.scala
@@ -1,15 +1,22 @@
 package org.scalatra
 
+import java.util.concurrent.Executors
+
 import _root_.akka.actor._
+import org.eclipse.jetty.server.{ Connector, ServerConnector, Server }
+import org.eclipse.jetty.util.thread.QueuedThreadPool
+import org.scalatra.test.HttpComponentsClient
 import org.scalatra.test.specs2.MutableScalatraSpec
 
-import scala.concurrent.Future
+import scala.concurrent._
 import scala.concurrent.duration._
 
 class AkkaSupportServlet extends ScalatraServlet with FutureSupport {
   val system = ActorSystem()
   protected implicit val executor = system.dispatcher
   override def asyncTimeout = 2 seconds
+
+  private val futureEC = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
 
   get("/redirect") {
     new AsyncResult {
@@ -25,6 +32,17 @@ class AkkaSupportServlet extends ScalatraServlet with FutureSupport {
         Thread.sleep(100) // To get the container to give up the request
         Ok(body = s"${request.getContextPath}")
       }
+    }
+  }
+
+  get("/async-attributes/:mockSessionId") {
+    request.setAttribute("sessionId", params("mockSessionId"))
+    val handlingReq = request
+    new AsyncResult {
+      override val is = Future {
+        Thread.sleep(200)
+        Ok(body = request.getAttribute("sessionId"))
+      }(futureEC)
     }
   }
 
@@ -77,6 +95,20 @@ class AkkaSupportServlet extends ScalatraServlet with FutureSupport {
 class AkkaSupportSpec extends MutableScalatraSpec {
   sequential
 
+  override lazy val server = {
+    /*
+     Min threads for Jetty is 6 because: acceptors=1 + selectors=4 + request=1
+
+     so 16 max and 6 min -> 10 worker threads
+      */
+    val threadPool = new QueuedThreadPool(16, 6)
+    val server = new Server(threadPool)
+    val connector: ServerConnector = new ServerConnector(server)
+    connector.setPort(port)
+    server.setConnectors(Array[Connector](connector))
+    server
+  }
+
   addServlet(new AkkaSupportServlet, "/*")
 
   "The AkkaSupport" should {
@@ -128,6 +160,31 @@ class AkkaSupportSpec extends MutableScalatraSpec {
       get("/async-oh-noes") {
         body must_== ""
         // body must not be_== "null"
+      }
+    }
+
+    "should not leak attributes between requests" in {
+      implicit val multiClentEc = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(50))
+      val ids = (1 to 50).map(_ => scala.util.Random.nextInt())
+      val serverBaseUrl = baseUrl
+      val idsToResponseFs = ids.map { id =>
+        val client = new HttpComponentsClient {
+          override val baseUrl: String = serverBaseUrl
+        }
+        Future {
+          blocking {
+            id.toString -> client.get(s"/async-attributes/$id") {
+              client.body
+            }
+          }
+        }(multiClentEc)
+      }
+      val fIdsToresponses = Future.sequence(idsToResponseFs)
+      val idsToResponses = Await.result(fIdsToresponses, 60.seconds)
+      foreachWhen(idsToResponses) {
+        case (expected, actual) => {
+          expected must_== actual
+        }
       }
     }
   }
