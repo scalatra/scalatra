2874ec1defca3f4bdb72c60a1b72be4495db07f4
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2015-08-09 23:32:13 +0200

    Dont deprecate AsyncResult, since it can be required in certain circumstances.

--- core/src/main/scala/org/scalatra/FutureSupport.scala
+++ core/src/main/scala/org/scalatra/FutureSupport.scala
@@ -10,7 +10,6 @@ import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.{ Failure, Success }
 
-@deprecated("AsyncResult is no longer required and a Future in an action can safely close over request/response.", "2.4.0")
 abstract class AsyncResult(
   implicit override val scalatraContext: ScalatraContext)
     extends ScalatraContext {
