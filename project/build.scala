c66472db31f31011177162e70526a0d59d90d74a
Naoki Takezoe <takezoe@gmail.com>
2016-11-03 13:30:58 +0900

    Restore commented out code about Scalate to pass compilation

--- project/build.scala
+++ project/build.scala
@@ -107,7 +107,7 @@ object ScalatraBuild extends Build {
       description := "Atmosphere integration for scalatra"
     )
   ) dependsOn(scalatraJson % "compile;test->test;provided->provided")
-/*
+
   lazy val scalatraScalate = Project(
     id = "scalatra-scalate",
     base = file("scalate"),
@@ -116,7 +116,7 @@ object ScalatraBuild extends Build {
       description := "Scalate integration with Scalatra"
     )
   ) dependsOn(scalatraCore % "compile;test->test;provided->provided")
-*/
+
   lazy val scalatraJson = Project(
     id = "scalatra-json",
     base = file("json"),
@@ -318,9 +318,9 @@ object ScalatraBuild extends Build {
     lazy val logbackClassic           =  "ch.qos.logback"          %  "logback-classic"            % "1.1.7"
     lazy val mimeUtil                 =  "eu.medsea.mimeutil"      %  "mime-util"                  % "2.1.3" exclude("org.slf4j", "slf4j-log4j12") exclude("log4j", "log4j")
     lazy val mockitoAll               =  "org.mockito"             %  "mockito-all"                % "1.10.19"
-    lazy val scalate                  =  "org.scalatra.scalate"    %% "scalate-core"               % scalateVersion
+    lazy val scalate                  =  "org.scalatra.scalate"    %% "scalate-core"               % scalateVersion // TODO Scala 2.12
     lazy val rl                       =  "org.scalatra.rl"         %  "rl_2.11"                    % "0.4.10" // TODO Scala 2.12
-    lazy val scalatest                =  "org.scalatest"           %% "scalatest"                  % scalatestVersion // TODO Scala 2.12
+    lazy val scalatest                =  "org.scalatest"           %% "scalatest"                  % scalatestVersion
     lazy val scalaz                   =  "org.scalaz"              %% "scalaz-core"                % "7.3.0-M5"
     lazy val servletApi               =  "javax.servlet"           %  "javax.servlet-api"          % "3.1.0"
     lazy val springWeb                =  "org.springframework"     %  "spring-web"                 % "4.2.3.RELEASE"
