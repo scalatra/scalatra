6fade56e1dbb4143f53eedc8b5680ab626ee450b
xuwei-k <6b656e6a69@gmail.com>
2017-01-07 20:12:56 +0900

    use Map#getOrElse

--- core/src/main/scala/org/scalatra/CORSSupport.scala
+++ core/src/main/scala/org/scalatra/CORSSupport.scala
@@ -119,7 +119,7 @@ trait CorsSupport extends Handler with Initializable { self: ScalatraBase ⇒
     val hdr = if (anyOriginAllowed && !corsConfig.allowCredentials)
       AnyOrigin
     else
-      request.headers.get(OriginHeader).getOrElse("")
+      request.headers.getOrElse(OriginHeader, "")
 
     response.headers(AccessControlAllowOriginHeader) = hdr
     if (corsConfig.allowCredentials) response.headers(AccessControlAllowCredentialsHeader) = "true"
@@ -132,11 +132,11 @@ trait CorsSupport extends Handler with Initializable { self: ScalatraBase ⇒
 
   private[this] def originMatches: Boolean = // 6.2.2
     corsConfig.allowedOrigins.contains(AnyOrigin) ||
-      (corsConfig.allowedOrigins contains request.headers.get(OriginHeader).getOrElse(""))
+      (corsConfig.allowedOrigins contains request.headers.getOrElse(OriginHeader, ""))
 
   private[this] def isEnabled: Boolean =
-    !("Upgrade".equalsIgnoreCase(request.headers.get("Connection").getOrElse("")) &&
-      "WebSocket".equalsIgnoreCase(request.headers.get("Upgrade").getOrElse(""))) &&
+    !("Upgrade".equalsIgnoreCase(request.headers.getOrElse("Connection", "")) &&
+      "WebSocket".equalsIgnoreCase(request.headers.getOrElse("Upgrade", ""))) &&
       !requestPath.contains("eb_ping") // don't do anything for the ping endpoint
 
   private[this] def isValidRoute: Boolean = routes.matchingMethods(requestPath).nonEmpty
