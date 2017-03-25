83d3ecf8717419de3370a5d89667ac6bc557cde6
Naoki Takezoe <takezoe@gmail.com>
2017-03-25 15:34:49 +0900

    Pass options to url() as named argument

--- core/src/main/scala/org/scalatra/ScalatraBase.scala
+++ core/src/main/scala/org/scalatra/ScalatraBase.scala
@@ -735,7 +735,11 @@ trait ScalatraBase
       implicit request: HttpServletRequest, response: HttpServletResponse): String = {
     if (path.startsWith("http")) path
     else {
-      val p = url(path, params, includeContextPath, includeServletPath, withSessionId = withSessionId)
+      val p = url(path, params,
+        includeContextPath = includeContextPath,
+        includeServletPath = includeServletPath,
+        withSessionId = withSessionId
+      )
       if (p.startsWith("http")) p else buildBaseUrl + ensureSlash(p)
     }
   }
