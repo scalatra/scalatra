af3b987eac3854c0ca54ff695f90102acf70dc4e
Naoki Takezoe <takezoe@gmail.com>
2017-06-26 01:29:31 +0900

    Convert RicherString to implicit value class from implicit conversion

--- core/src/main/scala/org/scalatra/util/RicherString.scala
+++ core/src/main/scala/org/scalatra/util/RicherString.scala
@@ -3,45 +3,49 @@ package org.scalatra.util
 import java.nio.charset.Charset
 import java.util.regex.Pattern
 
-class RicherString(orig: String) {
+package object RicherString {
 
-  def isBlank: Boolean = {
-    orig == null || orig.trim.isEmpty
-  }
+  implicit final class RicherStringImplicitClass(private val orig: String) extends AnyVal {
 
-  def blankOption: Option[String] = if (isBlank) None else Some(orig)
-  def nonBlank: Boolean = !isBlank
+    def isBlank: Boolean = {
+      orig == null || orig.trim.isEmpty
+    }
 
-  def urlEncode: String = UrlCodingUtils.urlEncode(orig)
-  def formEncode: String = UrlCodingUtils.urlEncode(orig, spaceIsPlus = true)
-  def urlDecode: String = UrlCodingUtils.urlDecode(orig)
-  def formDecode: String = UrlCodingUtils.urlDecode(orig, plusIsSpace = true)
+    def blankOption: Option[String] = if (isBlank) None else Some(orig)
+    def nonBlank: Boolean = !isBlank
 
-  def urlEncode(charset: Charset): String = UrlCodingUtils.urlEncode(orig, charset)
-  def formEncode(charset: Charset): String = UrlCodingUtils.urlEncode(orig, charset, spaceIsPlus = true)
-  def urlDecode(charset: Charset): String = UrlCodingUtils.urlDecode(orig, charset)
-  def formDecode(charset: Charset): String = UrlCodingUtils.urlDecode(orig, charset, plusIsSpace = true)
+    def urlEncode: String = UrlCodingUtils.urlEncode(orig)
+    def formEncode: String = UrlCodingUtils.urlEncode(orig, spaceIsPlus = true)
+    def urlDecode: String = UrlCodingUtils.urlDecode(orig)
+    def formDecode: String = UrlCodingUtils.urlDecode(orig, plusIsSpace = true)
 
-  def /(path: String): String = {
-    (orig.endsWith("/"), path.startsWith("/")) match {
-      case (true, false) | (false, true) ⇒ orig + path
-      case (false, false) ⇒ orig + "/" + path
-      case (true, true) ⇒ orig + path substring 1
+    def urlEncode(charset: Charset): String = UrlCodingUtils.urlEncode(orig, charset)
+    def formEncode(charset: Charset): String = UrlCodingUtils.urlEncode(orig, charset, spaceIsPlus = true)
+    def urlDecode(charset: Charset): String = UrlCodingUtils.urlDecode(orig, charset)
+    def formDecode(charset: Charset): String = UrlCodingUtils.urlDecode(orig, charset, plusIsSpace = true)
+
+    def /(path: String): String = {
+      (orig.endsWith("/"), path.startsWith("/")) match {
+        case (true, false) | (false, true) ⇒ orig + path
+        case (false, false) ⇒ orig + "/" + path
+        case (true, true) ⇒ orig + path substring 1
+      }
+    }
+
+    def regexEscape: String = Pattern.quote(orig)
+
+    def toCheckboxBool: Boolean = {
+      orig.toUpperCase match {
+        case "ON" | "TRUE" | "OK" | "1" | "CHECKED" | "YES" | "ENABLE" | "ENABLED" => true
+        case _ => false
+      }
     }
   }
 
-  def regexEscape: String = Pattern.quote(orig)
-
-  def toCheckboxBool: Boolean = {
-    orig.toUpperCase match {
-      case "ON" | "TRUE" | "OK" | "1" | "CHECKED" | "YES" | "ENABLE" | "ENABLED" => true
-      case _ => false
-    }
-  }
 }
 
-object RicherString {
-
-  implicit def stringToRicherString(s: String): RicherString = new RicherString(s)
-
-}
+//object RicherString {
+//
+//  implicit def stringToRicherString(s: String): RicherString = new RicherString(s)
+//
+//}
