40766e58ca2b7ca2e53f3775e6d504acd8b0c0dc
Shani Elharrar <shani.elha@gmail.com>
2017-07-28 19:46:29 +0300

    ScalatraServlet RequestPath Spec: Removed test that is not valid anymore (encoded)

--- core/src/test/scala/org/scalatra/ScalatraServletRequestPathSpec.scala
+++ core/src/test/scala/org/scalatra/ScalatraServletRequestPathSpec.scala
@@ -14,11 +14,6 @@ class ScalatraServletRequestPathSpec extends WordSpec with MustMatchers {
       ScalatraServlet.requestPath("/test;test/", 0) must equal("/test")
     }
 
-    "be extracted properly from encoded url" in {
-      ScalatraServlet.requestPath("/%D1%82%D0%B5%D1%81%D1%82/", 5) must equal("/")
-      ScalatraServlet.requestPath("/%D1%82%D0%B5%D1%81%D1%82/%D1%82%D0%B5%D1%81%D1%82/", 5) must equal("/тест/")
-    }
-
     "be extracted properly from decoded url" in {
       ScalatraServlet.requestPath("/тест/", 5) must equal("/")
       ScalatraServlet.requestPath("/тест/тест/", 5) must equal("/тест/")
