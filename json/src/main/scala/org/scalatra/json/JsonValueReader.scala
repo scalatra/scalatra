c7b56615757d8bea1ce8f6c78979c139de301a3a
xuwei-k <6b656e6a69@gmail.com>
2018-01-06 20:43:36 +0900

    avoid structural subtyping

--- json/src/main/scala/org/scalatra/json/JsonValueReader.scala
+++ json/src/main/scala/org/scalatra/json/JsonValueReader.scala
@@ -7,22 +7,23 @@ import org.scalatra.util.ValueReader
 
 import scala.util.control.Exception.allCatch
 
+object JsonValueReader {
+  private val separatorBeginning = "."
+  private val separatorEnd = ""
+}
+
 class JsonValueReader(val data: JValue)(implicit formats: Formats) extends ValueReader[JValue, JValue] {
   //  type I = T
-
-  private val separator = new {
-    val beginning = "."
-    val end = ""
-  }
+  import JsonValueReader._
 
   def read(key: String): Either[String, Option[JValue]] =
     allCatch.withApply(t => Left(t.getMessage)) { Right(readPath(key)) }
 
   protected def readPath(path: String, subj: JValue = data): Option[JValue] = {
-    val partIndex = path.indexOf(separator.beginning)
-    val (part, rest) = if (path.indexOf(separator.beginning) > -1) path.splitAt(partIndex) else (path, "")
+    val partIndex = path.indexOf(separatorBeginning)
+    val (part, rest) = if (path.indexOf(separatorBeginning) > -1) path.splitAt(partIndex) else (path, "")
     val realRest = if (rest.nonEmpty) {
-      if (separator.end.nonBlank) {
+      if (separatorEnd.nonBlank) {
         if (rest.size > 1) rest.substring(2) else rest.substring(1)
       } else rest.substring(1)
     } else rest
