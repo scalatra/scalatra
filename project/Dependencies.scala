a5c64f6c904719a0cf6ae71f75c243e12086628e
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2020-11-27 01:53:48 +0000

    Update scalatest to 3.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
   private val jettyVersion            = "9.4.29.v20200521"
   private val json4sVersion           = "3.6.8"
   private val scalateVersion          = "1.9.6"
-  private val scalatestVersion        = "3.1.2"
+  private val scalatestVersion        = "3.1.4"
   private val specs2Version           = "4.9.4"
   private val parserCombinatorVersion = Def.setting(
     CrossVersion.partialVersion(scalaVersion.value) match {
