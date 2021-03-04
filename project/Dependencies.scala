d20ab87e7d264ed3774c8187fa51cf9b243d838a
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-03-04 03:04:52 +0000

    Update scalatest to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,6 +54,6 @@ object Dependencies {
   private val jettyVersion            = "9.4.35.v20201120"
   private val json4sVersion           = "3.6.10"
   private val scalateVersion          = "1.9.6"
-  private val scalatestVersion        = "3.2.3"
+  private val scalatestVersion        = "3.2.5"
   private val specs2Version           = "4.10.5"
 }
