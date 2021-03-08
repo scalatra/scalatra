14fec2fb24e89726bf9bb7cf488aa8bb5028777f
xuwei-k <6b656e6a69@gmail.com>
2021-03-08 12:57:29 +0900

    add Scala 3 build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 import Keys._
 
 object Dependencies {
-  lazy val parserCombinators        =  "org.scala-lang.modules"  %% "scala-parser-combinators"   % "1.1.2"
-  lazy val xml                      =  "org.scala-lang.modules"  %% "scala-xml"                  % "1.3.0"
-  lazy val akkaActor                =  "com.typesafe.akka"       %% "akka-actor"                 % akkaVersion
-  lazy val akkaTestkit              =  "com.typesafe.akka"       %% "akka-testkit"               % akkaVersion
+  lazy val parserCombinators        =  "org.scala-lang.modules"  %% "scala-parser-combinators"   % "1.1.2" cross CrossVersion.for3Use2_13
+  lazy val xml                      =  "org.scala-lang.modules"  %% "scala-xml"                  % "1.3.0" cross CrossVersion.for3Use2_13
+  lazy val akkaActor                =  "com.typesafe.akka"       %% "akka-actor"                 % akkaVersion cross CrossVersion.for3Use2_13
+  lazy val akkaTestkit              =  "com.typesafe.akka"       %% "akka-testkit"               % akkaVersion cross CrossVersion.for3Use2_13
   lazy val atmosphereRuntime        =  "org.atmosphere"          %  "atmosphere-runtime"         % "2.6.4"
   lazy val atmosphereJQuery         =  "org.atmosphere.client"   %  "jquery"                     % "2.2.21" artifacts(Artifact("jquery", "war", "war"))
   lazy val atmosphereClient         =  "org.atmosphere"          %  "wasync"                     % "2.1.7"
@@ -23,17 +23,17 @@ object Dependencies {
   lazy val jettyServlet             =  "org.eclipse.jetty"       %  "jetty-servlet"              % jettyVersion
   lazy val jettyWebsocket           =  "org.eclipse.jetty.websocket" %"websocket-server"         % jettyVersion
   lazy val jettyWebapp              =  "org.eclipse.jetty"       %  "jetty-webapp"               % jettyVersion
-  lazy val json4sCore               =  "org.json4s"              %% "json4s-core"                % json4sVersion
-  lazy val json4sExt                =  "org.json4s"              %% "json4s-ext"                 % json4sVersion
-  lazy val json4sJackson            =  "org.json4s"              %% "json4s-jackson"             % json4sVersion
-  lazy val json4sNative             =  "org.json4s"              %% "json4s-native"              % json4sVersion
-  lazy val json4sXml                =  "org.json4s"              %% "json4s-xml"                 % json4sVersion
+  lazy val json4sCore               =  "org.json4s"              %% "json4s-core"                % json4sVersion cross CrossVersion.for3Use2_13
+  lazy val json4sExt                =  "org.json4s"              %% "json4s-ext"                 % json4sVersion cross CrossVersion.for3Use2_13
+  lazy val json4sJackson            =  "org.json4s"              %% "json4s-jackson"             % json4sVersion cross CrossVersion.for3Use2_13
+  lazy val json4sNative             =  "org.json4s"              %% "json4s-native"              % json4sVersion cross CrossVersion.for3Use2_13
+  lazy val json4sXml                =  "org.json4s"              %% "json4s-xml"                 % json4sVersion cross CrossVersion.for3Use2_13
   lazy val junit                    =  "junit"                   %  "junit"                      % "4.13.2"
   lazy val scalatestJunit           =  "org.scalatestplus"       %% "junit-4-13"                 % "3.2.5.0"
   lazy val jUniversalChardet        =  "com.github.albfernandez" %  "juniversalchardet"          % "2.4.0"
   lazy val logbackClassic           =  "ch.qos.logback"          %  "logback-classic"            % "1.2.3"
   lazy val mockitoAll               =  "org.mockito"             %  "mockito-core"               % "3.8.0"
-  lazy val scalate                  =  "org.scalatra.scalate"    %% "scalate-core"               % scalateVersion
+  lazy val scalate                  =  "org.scalatra.scalate"    %% "scalate-core"               % scalateVersion cross CrossVersion.for3Use2_13
   lazy val scalatest                =  "org.scalatest"           %% "scalatest"                  % scalatestVersion
   lazy val servletApi               =  "javax.servlet"           %  "javax.servlet-api"          % "3.1.0"
   lazy val slf4jApi                 =  "org.slf4j"               %  "slf4j-api"                  % "1.7.30"
@@ -41,12 +41,12 @@ object Dependencies {
                                        "org.specs2"              %% "specs2-core",
                                        "org.specs2"              %% "specs2-mock",
                                        "org.specs2"              %% "specs2-matcher-extra"
-                                                                                  ).map(_        % specs2Version)
-  lazy val metricsScala             =  "nl.grons"                %% "metrics4-scala"             % "4.1.14"
+                                       ).map(_ % specs2Version cross CrossVersion.for3Use2_13)
+  lazy val metricsScala             =  "nl.grons"                %% "metrics4-scala"             % "4.1.14" cross CrossVersion.for3Use2_13
   lazy val metricsServlets          =  "io.dropwizard.metrics"   %  "metrics-servlets"           % "4.1.18"
   lazy val metricsServlet           =  "io.dropwizard.metrics"   %  "metrics-servlet"            % "4.1.18"
   lazy val googleGuava              =  "com.google.guava"        %  "guava"                      % "30.1-jre"
-  lazy val twirlApi                 =  "com.typesafe.play"       %% "twirl-api"                  % "1.5.0"
+  lazy val twirlApi                 =  "com.typesafe.play"       %% "twirl-api"                  % "1.5.0" cross CrossVersion.for3Use2_13
 
   private val akkaVersion             = "2.6.13"
   private val atmosphereCompatVersion = "2.0.1"
