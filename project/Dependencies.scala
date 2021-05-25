75a9b00686b104b1f2f66672a3ada320086e7028
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-05-25 23:07:02 +0000

    Update json4s-core, json4s-ext, ... to 4.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
   private val atmosphereCompatVersion = "2.0.1"
   private val httpcomponentsVersion   = "4.5.6"
   private val jettyVersion            = "9.4.41.v20210516"
-  private val json4sVersion           = "3.6.11"
+  private val json4sVersion           = "4.0.0"
   private val scalateVersion          = "1.9.6"
   private val specs2Version           = "4.12.0"
   private val scalatestVersion        = "3.2.9"
