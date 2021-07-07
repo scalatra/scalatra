771a29b42089728a535409006ae02acba73ce02d
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-07-07 01:51:07 +0000

    Update websocket-server to 9.4.43.v20210629

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   private val akkaVersion             = "2.6.15"
   private val atmosphereCompatVersion = "2.0.1"
   private val httpcomponentsVersion   = "4.5.6"
-  private val jettyVersion            = "9.4.42.v20210604"
+  private val jettyVersion            = "9.4.43.v20210629"
   private val json4sVersion           = "4.0.1"
   private val scalateVersion          = "1.9.7"
   private val specs2Version           = "4.12.3"
