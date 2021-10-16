d8bc8b8d1cebe139fc27104da84418b50c1dcdad
Magnolia.K <magnolia.k@icloud.com>
2021-10-16 20:01:30 +0900

    Removed atmosphere module (#1302)
    atmosphere is an obstacle to Jetty updates due to infrequent updates
    To improve maintainability in the future, remove it.

--- atmosphere/src/main/scala/org/scalatra/atmosphere/NativeSimpleWireFormat.scala
+++
@@ -1,7 +0,0 @@
-package org.scalatra.atmosphere
-
-import org.json4s._
-
-class NativeSimpleWireFormat(implicit formats: Formats) extends SimpleJsonWireFormat with native.JsonMethods {
-  protected def renderJson(json: JsonAST.JValue): String = compact(render(json))
-}
