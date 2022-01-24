b1f40b6261c9bf76d314fc32fd5545a59a44b48c
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-01-24 05:00:45 +0000

    Update junit-4-13 to 3.2.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   lazy val json4sNative             =  "org.json4s"              %% "json4s-native"              % json4sVersion
   lazy val json4sXml                =  "org.json4s"              %% "json4s-xml"                 % json4sVersion
   lazy val junit                    =  "junit"                   %  "junit"                      % "4.13.2"
-  lazy val scalatestJunit           =  "org.scalatestplus"       %% "junit-4-13"                 % "3.2.10.0"
+  lazy val scalatestJunit           =  "org.scalatestplus"       %% "junit-4-13"                 % "3.2.11.0"
   lazy val jUniversalChardet        =  "com.github.albfernandez" %  "juniversalchardet"          % "2.4.0"
   lazy val logbackClassic           =  "ch.qos.logback"          %  "logback-classic"            % "1.2.10"
   lazy val mockitoAll               =  "org.mockito"             %  "mockito-core"               % "4.1.0"
