e7bfa4979f6304625f9445216a1e6b7d2a4d8185
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-01-24 13:09:56 +0000

    Update mockito-core to 4.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   lazy val scalatestJunit           =  "org.scalatestplus"       %% "junit-4-13"                 % "3.2.10.0"
   lazy val jUniversalChardet        =  "com.github.albfernandez" %  "juniversalchardet"          % "2.4.0"
   lazy val logbackClassic           =  "ch.qos.logback"          %  "logback-classic"            % "1.2.10"
-  lazy val mockitoAll               =  "org.mockito"             %  "mockito-core"               % "4.1.0"
+  lazy val mockitoAll               =  "org.mockito"             %  "mockito-core"               % "4.3.0"
   lazy val scalatest                =  Seq(
                                          "funspec",
                                          "wordspec",
