dd6bc76f9d91bdf9b07b93f893887adebeb059a6
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-08-09 04:42:27 +0000

    Update jakarta.servlet-api to 6.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
                                          "shouldmatchers",
                                          "mustmatchers",
                                        ).map(x => "org.scalatest" %% s"scalatest-$x" % scalatestVersion)
-  lazy val servletApi               =  "jakarta.servlet"         %  "jakarta.servlet-api"        % "5.0.0"
+  lazy val servletApi               =  "jakarta.servlet"         %  "jakarta.servlet-api"        % "6.0.0"
   lazy val slf4jApi                 =  "org.slf4j"               %  "slf4j-api"                  % "1.7.36"
   lazy val specs2                   =  Seq(
                                        "org.specs2"              %% "specs2-core",
