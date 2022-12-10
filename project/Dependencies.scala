4c4ae5cc4e9ef02bba52e63cb40fc77258520c8c
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-12-10 06:58:02 +0000

    Update httpclient, httpmime to 4.5.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,7 +46,7 @@ object Dependencies {
   lazy val googleGuava              =  "com.google.guava"        %  "guava"                      % "31.1-jre"
   lazy val twirlApi                 =  "com.typesafe.play"       %% "twirl-api"                  % "1.6.0-M7"
 
-  private val httpcomponentsVersion   = "4.5.6"
+  private val httpcomponentsVersion   = "4.5.14"
   private val jettyVersion            = "10.0.13"
   private val json4sVersion           = "4.0.6"
   private val specs2Version           = "4.19.0"
