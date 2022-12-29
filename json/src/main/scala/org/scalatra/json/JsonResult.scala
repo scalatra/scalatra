736fd10ed5c89701a0c37a7f09c2e03c4ec91320
kenji yoshida <6b656e6a69@gmail.com>
2022-12-29 09:55:52 +0900

    fix warning in JsonResult.scala
    ```
    [warn] -- Migration Warning: /home/runner/work/scalatra/scalatra/json/src/main/scala/org/scalatra/json/JsonResult.scala:8:71 
    [warn] 8 |  def apply[T](v: T)(implicit T: T => JValue): JsonResult = JsonResult(v: JValue)
    [warn]   |                                                                       ^
    [warn]   |The conversion (T : T => org.json4s.JValue) will not be applied implicitly here in Scala 3 because only implicit methods and instances of Conversion class will continue to work as implicit views.
    ```

--- json/src/main/scala/org/scalatra/json/JsonResult.scala
+++ json/src/main/scala/org/scalatra/json/JsonResult.scala
@@ -5,5 +5,5 @@ import org.json4s.JValue
 case class JsonResult(value: JValue)
 
 object JsonResult {
-  def apply[T](v: T)(implicit T: T => JValue): JsonResult = JsonResult(v: JValue)
+  def apply[T](v: T)(implicit T: T => JValue): JsonResult = JsonResult(T.apply(v))
 }
