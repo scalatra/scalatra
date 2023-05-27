2888abcccf37b9bd60dd3b3f9df69f40bab511f0
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2023-05-27 00:00:32 +0000

    Update guava to 32.0.0-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
   lazy val metricsScala             =  "nl.grons"                %% "metrics4-scala"             % "4.2.9"
   lazy val metricsServlets          =  "io.dropwizard.metrics"   %  "metrics-servlets"           % "4.2.18"
   lazy val metricsServlet           =  "io.dropwizard.metrics"   %  "metrics-servlet"            % "4.2.18"
-  lazy val googleGuava              =  "com.google.guava"        %  "guava"                      % "31.1-jre"
+  lazy val googleGuava              =  "com.google.guava"        %  "guava"                      % "32.0.0-jre"
   lazy val twirlApi                 =  "com.typesafe.play"       %% "twirl-api"                  % "1.6.0-RC2"
 
   private val jettyVersion            = "10.0.15"
