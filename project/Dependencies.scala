9a011d40dfc399d50b087fefc39917c8c20ea7cf
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2023-07-11 00:06:27 +0000

    Update scala-xml to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object Dependencies {
   lazy val parserCombinators        =  "org.scala-lang.modules"  %% "scala-parser-combinators"   % "2.3.0"
-  lazy val xml                      =  "org.scala-lang.modules"  %% "scala-xml"                  % "2.1.0"
+  lazy val xml                      =  "org.scala-lang.modules"  %% "scala-xml"                  % "2.2.0"
   lazy val collectionCompact        =  "org.scala-lang.modules"  %% "scala-collection-compat"    % "2.11.0"
   lazy val commonsText              =  "org.apache.commons"      %  "commons-text"               % "1.10.0"
   lazy val httpclient               =  "org.apache.httpcomponents.client5" % "httpclient5"       % "5.2.1"
