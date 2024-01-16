df4afe7cc638018e85ee8cc8595f006d268236dc
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2024-01-16 10:54:54 +0000

    Update specs2-core to 4.20.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,6 +45,6 @@ object Dependencies {
   lazy val twirlApi                 =  "org.playframework.twirl"       %% "twirl-api"                  % "2.0.3"
 
   private val json4sVersion           = "4.0.7"
-  private val specs2Version           = "4.20.3"
+  private val specs2Version           = "4.20.4"
   private val scalatestVersion        = "3.2.17"
 }
