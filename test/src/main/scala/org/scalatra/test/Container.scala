3f969273dd4d55292c333f48a63221fc2e7709f9
ttj4 <jinto.thomas@live.com>
2025-02-27 20:21:45 +0530

    1786: ensuring resources with file protocols are resolved properly

--- test/src/main/scala/org/scalatra/test/Container.scala
+++ test/src/main/scala/org/scalatra/test/Container.scala
@@ -1,13 +1,18 @@
 package org.scalatra.test
 
 import java.nio.file.{Path, Paths}
+import scala.jdk.CollectionConverters.*
 
 trait Container {
   protected def ensureSessionIsSerializable(): Unit
   protected def start(): Unit
   protected def stop(): Unit
   var resourceBasePath: Path = {
-    val projectPath = Paths.get(getClass.getClassLoader.getResource("").toURI.resolve("../../.."))
+    val urls = getClass.getClassLoader.getResources("").asScala.toSeq
+    val fileUrl = urls.find(_.getProtocol == "file").getOrElse(
+      throw new IllegalStateException("No file URL found in class loader resources")
+    )
+    val projectPath = Paths.get(fileUrl.toURI).resolve("../../..")
     val webAppPath  = projectPath.resolve("src/main/webapp")
     if (webAppPath.toFile.isDirectory) {
       webAppPath
