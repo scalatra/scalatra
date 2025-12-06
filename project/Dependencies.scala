e9bcec80f30e23bbad0a261b1917960f0686c6af
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2025-12-06 07:13:16 +0000

    Update jetty-ee10-webapp to 12.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val jettyServletJavax   = "org.eclipse.jetty"                 % "jetty-servlet"            % "10.0.21"
   lazy val jettyWebappJavax    = "org.eclipse.jetty"                 % "jetty-webapp"             % "10.0.26"
   lazy val jettyServletJakarta = "org.eclipse.jetty.ee10"            % "jetty-ee10-servlet"       % "12.0.9"
-  lazy val jettyWebappJakarta  = "org.eclipse.jetty.ee10"            % "jetty-ee10-webapp"        % "12.0.31"
+  lazy val jettyWebappJakarta  = "org.eclipse.jetty.ee10"            % "jetty-ee10-webapp"        % "12.1.5"
   lazy val json4sCore          = "org.json4s"                       %% "json4s-core"              % json4sVersion
   lazy val json4sJackson       = "org.json4s"                       %% "json4s-jackson"           % json4sVersion
   lazy val json4sNative        = "org.json4s"                       %% "json4s-native"            % json4sVersion
