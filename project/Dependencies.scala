44877dc343f99c565b25d1b5aabc70fa60dcc668
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2026-01-26 03:07:33 +0000

    Update metrics-jakarta-servlet, ... to 4.2.38

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -37,10 +37,10 @@ object Dependencies {
     "org.specs2" %% "specs2-matcher-extra"
   ).map(_ % specs2Version)
   lazy val metricsScala           = "nl.grons"                %% "metrics4-scala"           % "4.3.7"
-  lazy val metricsServletsJavax   = "io.dropwizard.metrics"    % "metrics-servlets"         % "4.2.37"
-  lazy val metricsServletJavax    = "io.dropwizard.metrics"    % "metrics-servlet"          % "4.2.37"
-  lazy val metricsServletsJakarta = "io.dropwizard.metrics"    % "metrics-jakarta-servlets" % "4.2.37"
-  lazy val metricsServletJakarta  = "io.dropwizard.metrics"    % "metrics-jakarta-servlet"  % "4.2.37"
+  lazy val metricsServletsJavax   = "io.dropwizard.metrics"    % "metrics-servlets"         % "4.2.38"
+  lazy val metricsServletJavax    = "io.dropwizard.metrics"    % "metrics-servlet"          % "4.2.38"
+  lazy val metricsServletsJakarta = "io.dropwizard.metrics"    % "metrics-jakarta-servlets" % "4.2.38"
+  lazy val metricsServletJakarta  = "io.dropwizard.metrics"    % "metrics-jakarta-servlet"  % "4.2.38"
   lazy val googleGuava            = "com.google.guava"         % "guava"                    % "33.4.7-jre"
   lazy val twirlApi               = "org.playframework.twirl" %% "twirl-api"                % "2.0.9"
 
